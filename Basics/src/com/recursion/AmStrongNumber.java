f8e3aa14474f1d64afd0e70b6fb2ae00f1ac79cb
Soumitra jena <131749408+somjena@users.noreply.github.com>
2024-10-17 00:18:01 +0530

    Amstrong Number Using Recursion

---
+++ Basics/src/com/recursion/AmStrongNumber.java
@@ -0,0 +1,31 @@
+package com.recursion;
+import java.util.*;
+public class AmStrongNumber {
+	static int count;
+	
+	public static int AmStrong(int n) {
+		if(n==0)return 0;
+		return (int)Math.pow(n%10, count)+AmStrong(n/10);
+	}
+	
+	
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter The Number");
+		int num =sc.nextInt();
+		int Temp = num;
+		int RealNum = num;
+		while(num!=0) {
+			num/=10;
+			count++;
+		}
+		int res = AmStrong(RealNum);
+		if(res==Temp) {
+			System.out.println(Temp + " Is A AmStrong Number");
+		}else {
+			System.out.println(Temp + " Is Not A Amstrong Number");
+		}
+	}
+
+}
