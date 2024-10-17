be13827d5ddab11e1143848e86139f19e62b2aa1
Soumitra jena <131749408+somjena@users.noreply.github.com>
2024-10-18 00:31:39 +0530

    Reverse Number

---
+++ Basics/src/com/recursion/ReverseNumber.java
@@ -0,0 +1,21 @@
+package com.recursion;
+import java.util.*;
+public class ReverseNumber {
+	private static int rev ;
+	
+	public static int Reverse(int n) {
+		if(n==0) return rev;
+		rev = rev*10+n%10;
+		return Reverse(n/=10);
+	}
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter The Number :-");
+		int n = sc.nextInt();
+		System.out.println(Reverse(n));
+		
+	
+	}
+
+}
