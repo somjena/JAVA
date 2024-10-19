c7233c37fb457dd935b24a9fd2939c79b0059c11
Soumitra jena <131749408+somjena@users.noreply.github.com>
2024-10-19 13:37:43 +0530

    Super class Vehicle Concept

---
+++ Oops/src/com/Abstraction/Vehicle.java
@@ -0,0 +1,56 @@
+package com.Abstraction;
+
+abstract public class Vehicle {
+	 abstract void start();
+	 abstract void acc();
+	 abstract void stop();
+
+}
+class BMW extends Vehicle{
+
+	@Override
+	void start() {
+		System.out.println("BMW Start");
+		
+	}
+
+	@Override
+	void acc() {
+		System.out.println("BMW Accelarate");
+		
+	}
+
+	@Override
+	void stop() {
+		System.out.println("BMW Stop");
+		
+	}
+	
+}
+class Benz extends Vehicle{
+
+	@Override
+	void start() {
+		System.out.println("BENZ Start");
+		
+	}
+
+	@Override
+	void acc() {
+		System.out.println("BENZ Accelarate");
+	}
+
+	@Override
+	void stop() {
+		System.out.println("BENZ Stop");
+	}
+	
+}
+
+class Driver{
+	public static void  Drive(Vehicle v) {
+		v.start();
+		v.acc();
+		v.stop();
+	}
+}
