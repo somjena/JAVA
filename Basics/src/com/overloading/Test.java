566ac53b49d59cb6c33ecfc58c8d15263fd9addc
Soumitra jena <131749408+somjena@users.noreply.github.com>
2024-10-20 20:38:52 +0530

    Method Overloading Example

---
+++ Basics/src/com/overloading/Test.java
@@ -0,0 +1,25 @@
+package com.overloading;
+class A{
+	
+	void m1(int i) {
+			System.out.println("Int Arg Method");
+		
+	}
+	void m1() {
+		System.out.println("No Arg Method");
+		
+	}
+	void m1(String name){
+		System.out.println("String Arg Methods");
+		
+	}
+}
+public class Test {
+	public static void main(String[] args) {
+		A a1 = new A();
+		a1.m1("Soumitra");
+		a1.m1(10);
+		a1.m1();
+	}
+
+}
