ba5b3d701d8300f1fbe62ea6068eba7fa9a0f50c
Soumitra jena <131749408+somjena@users.noreply.github.com>
2024-10-22 20:16:07 +0530

    Static Block Execute When The Class Loading Process Happening

---
+++ Basics/src/com/initializer/Multiline/Static_Bolcks.java
@@ -0,0 +1,22 @@
+package com.initializer.Multiline;
+
+public class Static_Bolcks {
+	static {
+		
+		System.out.println("Static Block 1");
+	}
+	static {
+		System.out.println("Static Block 2");
+	}
+
+	public static void main(String[] args) {
+			
+		}
+	static {
+		System.out.println("Static block 3");
+	}
+		
+
+	}
+
+
