58fbd0accfcb314403bb26a618071f8fa1845351
Soumitra jena <131749408+somjena@users.noreply.github.com>
2024-10-22 20:10:36 +0530

    Update

--- Basics/src/com/initializer/SingleLine/Test.java
+++ Basics/src/com/initializer/SingleLine/Test.java
@@ -1,11 +1,14 @@
 package com.initializer.SingleLine;
 
 public class Test {
-	static int i = 10;
+	static int i = 10;   // Single Line Static Variable initializing  
+	int j =20;           // Single Line NonStatic Variable Initializing
 	public static void main(String[] args) {
 		int i =20;
-		System.out.println("Single Line InitiaLizer "+i);
-		System.out.println("Local Variable "+ Test.i);
+		System.out.println("Single Line InitiaLizer Static"+i);
+		System.out.println("Local Variable Local "+ Test.i);
+		Test t1 = new Test();
+		System.out.println("Non Static Single Line Initializing "+t1.j);
 		
 	}
 
