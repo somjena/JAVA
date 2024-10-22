0602918e26404ea0471b21b0e089ac13d37280b0
Soumitra jena <131749408+somjena@users.noreply.github.com>
2024-10-22 20:01:21 +0530

    Single Line Initializer Example

---
+++ Basics/src/com/initializer/SingleLine/Test.java
@@ -0,0 +1,12 @@
+package com.initializer.SingleLine;
+
+public class Test {
+	static int i = 10;
+	public static void main(String[] args) {
+		int i =20;
+		System.out.println("Single Line InitiaLizer "+i);
+		System.out.println("Local Variable "+ Test.i);
+		
+	}
+
+}
