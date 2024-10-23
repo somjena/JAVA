857bc927d24d3e75a64ca9b60c321d8f220e8e5a
Soumitra jena <131749408+somjena@users.noreply.github.com>
2024-10-23 19:52:45 +0530

    Question 1 - Positive or Negative

---
+++ Basics/src/com/Loops/PrepinstaQstn_100_Soln/Positive_or_Negative.java
@@ -0,0 +1,17 @@
+package com.Loops.PrepinstaQstn_100_Soln;
+import java.util.*;
+public class Positive_or_Negative {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter The Number : - ");
+		int num = sc.nextInt();
+		if(num<0) {
+			System.out.println(num + " is A Negative Number");
+		}else {
+			System.out.println(num + " is a Positive Number");
+		}
+		
+	}
+
+}
