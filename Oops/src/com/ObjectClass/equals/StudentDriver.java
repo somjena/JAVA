3744c6d5026b3e5dc581a308e371d0a9f467fe53
Soumitra jena <131749408+somjena@users.noreply.github.com>
2024-10-25 19:56:05 +0530

    Student String and Equals Method

---
+++ Oops/src/com/ObjectClass/equals/StudentDriver.java
@@ -0,0 +1,33 @@
+package com.ObjectClass.equals;
+class Student{
+	String name;
+	int age;
+	double dob;
+	Student(String name,int age,double dob){
+		this.name=name;
+		this.age=age;
+		this.dob=dob;
+	}
+	@Override
+	public String toString() {
+		return "name:-"+name+"\n"+"age:-"+age+"\n"+"dob:-"+dob;
+		
+	}
+	public boolean equals(Object o) {
+		Student s =(Student)o;
+		return this.name.equals(s.name)&& this.age==s.age && this.dob==s.dob;
+	}
+}
+public class StudentDriver {
+
+	public static void main(String[] args) {
+		Student s1= new Student("Som",22,22.24);
+		Student s2= new Student("Som",22,22.24);
+		
+		System.out.println(s1);
+		System.out.println(s2);
+		System.out.println(s1.equals(s2));
+
+	}
+
+}
