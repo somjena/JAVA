e435f9b55cc54fff9922ee9f0e9ce440c5c3741c
Soumitra jena <131749408+somjena@users.noreply.github.com>
2024-10-26 17:27:48 +0530

    EqualsignoreCase String Class Program

---
+++ Oops/src/com/StringPrograms/EqualsIgnoreCase.java
@@ -0,0 +1,13 @@
+package com.StringPrograms;
+
+public class EqualsIgnoreCase {
+
+	public static void main(String[] args) {
+		String p1 = "java";
+		String p2 = "JAVA";
+		System.out.println(p1.equals(p2));
+		System.out.println(p1.equalsIgnoreCase(p2));
+
+	}
+
+}
