5cefcbd1660b2b7867afe79fab45d034c1503691
Soumitra jena <131749408+somjena@users.noreply.github.com>
2024-10-26 17:44:07 +0530

    Length Method In java

---
+++ Oops/src/com/StringPrograms/Length_method.java
@@ -0,0 +1,17 @@
+package com.StringPrograms;
+
+public class Length_method {
+
+	public static void main(String[] args) {
+	String s1 = "Soumitra Jena";
+	System.out.println(s1.length());
+    Object o2 ="Bharath";
+    
+    System.out.println(((String)o2).length());
+	
+	String s2 = "India";
+
+	System.out.println(s2.length());
+	}
+
+}
