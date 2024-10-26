8a351c397d9e4be5c9cace726b18bc46807eaa8a
Soumitra jena <131749408+somjena@users.noreply.github.com>
2024-10-26 17:26:55 +0530

    String Class Programs

---
+++ Oops/src/com/StringPrograms/Demo.java
@@ -0,0 +1,19 @@
+package com.StringPrograms;
+
+public class Demo {
+
+	public static void main(String[] args) {
+	
+		String User1 =new String( "Soumitra");
+		System.out.println(User1);
+		
+		String User2= "Soumitra";
+		System.out.println(User2);
+		System.out.println(User1==User2);
+		System.out.println(User1.equals(User2));
+		
+		
+
+	}
+
+}
