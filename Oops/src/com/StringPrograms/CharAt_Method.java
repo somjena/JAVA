34a3f2fd93ca86fdd15ab15e380ae1701b0b3da7
Soumitra jena <131749408+somjena@users.noreply.github.com>
2024-10-26 17:33:32 +0530

    CharAt Method

---
+++ Oops/src/com/StringPrograms/CharAt_Method.java
@@ -0,0 +1,14 @@
+package com.StringPrograms;
+
+public class CharAt_Method {
+
+	public static void main(String[] args) {
+		String name = "Soumitra";
+		for(int i =0;i<=name.length()-1;i++) {
+			System.out.println(name.charAt(i));
+		}
+		
+
+	}
+
+}
