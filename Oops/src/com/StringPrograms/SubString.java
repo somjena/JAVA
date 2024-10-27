36b3b88fb3017acf18d092a5fa3edeafc9d5ab32
Soumitra jena <131749408+somjena@users.noreply.github.com>
2024-10-27 20:07:53 +0530

    Substring

---
+++ Oops/src/com/StringPrograms/SubString.java
@@ -0,0 +1,13 @@
+package com.StringPrograms;
+
+public class SubString {
+
+	public static void main(String[] args) {
+		String s1 = "javaDeveleoper";
+		System.out.println(s1.substring(0,4));
+		System.out.println(s1.substring(4));
+		
+
+	}
+
+}
