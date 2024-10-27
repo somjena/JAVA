b01428317cb9652c8a992cc98980d54de6c314eb
Soumitra jena <131749408+somjena@users.noreply.github.com>
2024-10-27 20:32:57 +0530

    AutoUnboxing

---
+++ Oops/src/com/Wrapperclass/AutoUnBoxing/User.java
@@ -0,0 +1,13 @@
+package com.Wrapperclass.AutoUnBoxing;
+
+public class User {
+
+	public static void main(String[] args) {
+		Integer age = 30;
+		int age1 = age;
+		Boolean Log = false;
+		boolean status = Log;
+
+	}
+
+}
