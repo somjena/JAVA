e7c643bfcbb2e308c3ac8e1042c0207dba1265ba
Soumitra jena <131749408+somjena@users.noreply.github.com>
2024-10-27 20:31:16 +0530

    AutoBoxing Conversion

---
+++ Oops/src/com/Wrapperclass/AutoBoxing/User.java
@@ -0,0 +1,15 @@
+package com.Wrapperclass.AutoBoxing;
+
+public class User {
+
+	public static void main(String[] args) {
+		int age=10;
+		Integer age1=age;
+		boolean log = true;
+		Boolean Login = false;
+		System.out.println(age1);
+		System.out.println(Login);
+
+	}
+
+}
