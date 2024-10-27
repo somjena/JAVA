70e45d1e19040dac5698894cb90c5ecb47e9f7bd
Soumitra jena <131749408+somjena@users.noreply.github.com>
2024-10-27 20:07:42 +0530

    tolowercase

---
+++ Oops/src/com/StringPrograms/ToLowercase.java
@@ -0,0 +1,15 @@
+package com.StringPrograms;
+
+import java.util.Scanner;
+
+public class ToLowercase {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the String");
+		String name = sc.nextLine();
+		System.out.println(name.toLowerCase());
+
+	}
+
+}
