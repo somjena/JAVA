c01a99e921683175dcc0c3e257302b1c1897daa3
Soumitra jena <131749408+somjena@users.noreply.github.com>
2024-10-27 20:26:29 +0530

    Single Line Initializer

---
+++ Basics/src/com/initializer/SingleLine/DemoTest.java
@@ -0,0 +1,16 @@
+package com.initializer.SingleLine;
+
+public class DemoTest {
+	int a= 20;
+	static int b=30;
+
+	public static void main(String[] args) {
+		int c= 30;
+		System.out.println(c);
+		DemoTest d1 = new DemoTest();
+		System.out.println(d1.a);
+		System.out.println(DemoTest.b);
+		
+	}
+
+}
