088d6ae8336a5b6d9dc92263ac5d2895bac9941a
Soumitra jena <131749408+somjena@users.noreply.github.com>
2024-10-29 00:15:13 +0530

    Cgar Array using For Each Loop

---
+++ Oops/src/com/StringPrograms/ToCharArray.java
@@ -0,0 +1,14 @@
+package com.StringPrograms;
+
+public class ToCharArray {
+
+	public static void main(String[] args) {
+		String s= "abcd";
+		char[]arr=s.toCharArray();
+		for(char c:arr) {
+			System.out.println(c);
+		}
+
+	}
+
+}
