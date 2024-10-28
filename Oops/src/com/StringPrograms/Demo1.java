510b23f5549fae5185a6fbd9f1cc1a5d0f4ae2fa
Soumitra jena <131749408+somjena@users.noreply.github.com>
2024-10-29 00:13:26 +0530

    toCharArray()

---
+++ Oops/src/com/StringPrograms/Demo1.java
@@ -0,0 +1,14 @@
+package com.StringPrograms;
+
+public class Demo1 {
+
+	public static void main(String[] args) {
+		String s= "Soumitra";
+		char [] arr=s.toCharArray();
+		for(int i=0;i<=s.length()-1;i++) {
+			System.out.println(s.charAt(i));
+		}
+
+	}
+
+}
