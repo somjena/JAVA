c2acb0da8f44455f889f63dc9c6c8e8bf757f5d9
Soumitra jena <131749408+somjena@users.noreply.github.com>
2024-10-29 14:13:53 +0530

    Encoding

---
+++ Oops/src/com/StringPrograms/Encoding.java
@@ -0,0 +1,23 @@
+package com.StringPrograms;
+
+public class Encoding {
+
+	public static void main(String[] args) {
+		String s = "jsp";
+		String endcodedstr = encode(s,15);
+		System.out.println(endcodedstr);
+		// TODO Auto-generated method stub
+
+	}
+	static String encode(String s,int l) {
+		char[]arr=s.toCharArray();
+		char[]letter="abcdefghijklmnopqrstuvwxyz".toCharArray();
+		
+		for(int i=0;i<=arr.length;i++) {
+			char c =arr[i];
+			 arr[i] = (char) (((c - 'a' + l) % 26) + 'a');
+		}
+		return new String(arr);
+	}
+
+}
