fbf354c5578b9d3721bb743793344168f38df951
Soumitra jena <131749408+somjena@users.noreply.github.com>
2024-10-30 21:54:26 +0530

    index of method

---
+++ Oops/src/com/StringPrograms/IndexOf_Method.java
@@ -0,0 +1,14 @@
+package com.StringPrograms;
+
+public class IndexOf_Method {
+
+	public static void main(String[] args) {
+		String s1 = "developer";
+		System.out.println(s1.indexOf('e'));
+		System.out.println(s1.indexOf('e',4));
+		System.out.println(s1.indexOf('d',5));
+		System.out.println(s1.indexOf('e'));
+
+	}
+
+}
