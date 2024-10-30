6308a0349055f0a0e070a369c69f02db4c93bdb7
Soumitra jena <131749408+somjena@users.noreply.github.com>
2024-10-30 12:05:14 +0530

    Capitalize Each letter Of A Sentence

---
+++ Oops/src/com/StringPrograms/Initcap.java
@@ -0,0 +1,20 @@
+package com.StringPrograms;
+
+public class Initcap {
+
+	public static void main(String[] args) {
+		System.out.println(convert("Soumitra kumar jena"));
+		
+
+	}
+	public static String convert(String s) {
+		char[]ch=s.toCharArray();
+		for(int i=0;i<ch.length;i++) {
+			if(ch[i]>='a'&&ch[i]<='z')
+			if(i==0||ch[i-1]==' ')
+				ch[i]=(char)(ch[i]-32);
+		}
+		return new String(ch);
+	}
+
+}
