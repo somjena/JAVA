0ae333c614cbe197223ef5087c51c8d8351f72ab
Soumitra jena <131749408+somjena@users.noreply.github.com>
2024-10-31 22:43:00 +0530

    Constructor

---
+++ Basics/src/com/constructor/Student.java
@@ -0,0 +1,25 @@
+package com.constructor;
+
+public class Student {
+
+    String name;
+    int age;
+
+    public Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public void displayInfo() {
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+    }
+
+    public static void main(String[] args) {
+     
+        Student student1 = new Student("Soumitra Jena", 24);
+
+       
+        student1.displayInfo();
+    }
+}
