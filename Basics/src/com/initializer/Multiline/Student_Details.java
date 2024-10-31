d959457d575e61d026c813c7d5611d3bc53e9a54
Soumitra jena <131749408+somjena@users.noreply.github.com>
2024-10-31 23:36:28 +0530

    MultiLine Initializer

---
+++ Basics/src/com/initializer/Multiline/Student_Details.java
@@ -0,0 +1,16 @@
+package com.initializer.Multiline;
+
+public class Student_Details {
+	static{
+		System.out.println("Static Block initialize");
+		
+	}
+
+	public static void main(String[] args) {
+		
+	}
+	static {
+		System.out.println("Static 2");
+	}
+
+}
