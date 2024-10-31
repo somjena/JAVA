d069e5e5cc9a6f816ee16c4a751d6e355a9c48b3
Soumitra jena <131749408+somjena@users.noreply.github.com>
2024-10-31 23:48:37 +0530

    compare to Method

---
+++ Oops/src/com/StringPrograms/CompareTo.java
@@ -0,0 +1,11 @@
+package com.StringPrograms;
+
+public class CompareTo {
+
+	public static void main(String[] args) {
+	String s1 = "Babu";
+	String s2 = "bali";
+	System.out.println(s1.compareTo(s2));
+	}
+
+}
