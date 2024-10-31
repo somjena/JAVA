a66024036e3680c3e8e6903ef01754a7217839b0
Soumitra jena <131749408+somjena@users.noreply.github.com>
2024-10-31 23:45:01 +0530

    wrapper class

---
+++ Oops/src/com/Wrapperclass/AutoBoxing/Student.java
@@ -0,0 +1,15 @@
+package com.Wrapperclass.AutoBoxing;
+
+public class Student {
+
+	public static void main(String[] args) {
+		int age = 22;
+		Integer userage = age;
+		String name = "Som";
+		String username =name;
+		System.out.println(username);
+		System.out.println(userage);
+
+	}
+
+}
