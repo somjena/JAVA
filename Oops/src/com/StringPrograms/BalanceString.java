a640227b1edd7f6d005f013081d2d053964637ee
Soumitra jena <131749408+somjena@users.noreply.github.com>
2024-11-02 13:10:28 +0530

    update

--- Oops/src/com/StringPrograms/BalanceString.java
+++ Oops/src/com/StringPrograms/BalanceString.java
@@ -9,20 +9,19 @@ public class BalanceString {
 
     public static boolean isBalanced(String arg) {
         String s = "";
-        // Collect only the brackets from the input
+      
         for (int i = 0; i < arg.length(); i++) {
             char c = arg.charAt(i);
             if (c == '[' || c == ']' || c == '{' || c == '}' || c == '(' || c == ')') {
                 s += c;
             }
         }
-        // Remove pairs iteratively
         while (s.contains("[]") || s.contains("{}") || s.contains("()")) {
             s = s.replace("[]", "");
             s = s.replace("{}", "");
             s = s.replace("()", "");
         }
-        // If all pairs are balanced, s should be empty
+      
         return s.length() == 0;
     }
 }
