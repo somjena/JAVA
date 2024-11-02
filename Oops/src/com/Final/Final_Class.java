db2d94da815db0ee3ce770c379240045fb2f6545
Soumitra jena <131749408+somjena@users.noreply.github.com>
2024-11-02 13:19:31 +0530

    Final Class in java

---
+++ Oops/src/com/Final/Final_Class.java
@@ -0,0 +1,21 @@
+package com.Final;
+class AA{
+	
+}
+final class BB extends AA{
+	
+}
+/*class CC extends BB{
+	Final Class Can not be a Parent for Any Child Class 
+}
+*/
+
+public class Final_Class {
+
+	public static void main(String[] args) {
+		
+		
+
+	}
+
+}
