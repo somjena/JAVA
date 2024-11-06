5e40a0889c2dbdfb5ffa099ed119b196f88af385
Soumitra jena <131749408+somjena@users.noreply.github.com>
2024-11-06 21:46:56 +0530

    Second Max Occurance

---
+++ Oops/src/com/StringPrograms/Second_max_occured_char.java
@@ -0,0 +1,21 @@
+package com.StringPrograms;
+
+public class Second_max_occured_char {
+
+	public static void main(String[] args) {
+		String s1 = "Hello world";
+		
+		
+		while(s1.length()>0) {
+			char c = s1.charAt(0);
+			String s2=s1.replace(c+"", "");
+			
+			int count =s1.length()-s2.length();
+			
+			System.out.println(c+"="+count);
+			s1=s2;
+		}
+
+	}
+
+}
