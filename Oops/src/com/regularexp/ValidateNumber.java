ab87613a5c813576df91aa1ab83ac7b46ee240ef
Soumitra jena <131749408+somjena@users.noreply.github.com>
2024-11-07 20:06:25 +0530

    update

--- Oops/src/com/regularexp/ValidateNumber.java
+++ Oops/src/com/regularexp/ValidateNumber.java
@@ -7,6 +7,7 @@ public class ValidateNumber {
 	public static void main(String[] args) {
 		System.out.println(isValidate("6371326214"));
 	}
+	
 	public static boolean isValidate(String s) {
 		String exp ="[6-9][0-9]{9}";
 		if(Pattern.matches(exp, s)){
