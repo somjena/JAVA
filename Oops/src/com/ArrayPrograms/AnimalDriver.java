f9cc2884eab8b352654cd3b63b8dcbcfa017bee7
Soumitra jena <131749408+somjena@users.noreply.github.com>
2024-11-07 20:51:10 +0530

    Animal Type of Array Example

---
+++ Oops/src/com/ArrayPrograms/AnimalDriver.java
@@ -0,0 +1,45 @@
+package com.ArrayPrograms;
+
+interface Animal{
+	void makeSound();
+}
+class Dog implements Animal{
+
+	@Override
+	public void makeSound() {
+		System.out.println("Bowwww.... Boww......");
+		
+	}
+	void jump() {
+		System.out.println("Jump......");
+	}
+	
+}
+class Cat implements Animal{
+
+	@Override
+	public void makeSound() {
+		System.out.println("Meoowww........");
+		
+	}
+	
+}
+public class AnimalDriver {
+
+	public static void main(String[] args) {
+		Animal [] n=new Animal[4];
+		n[0]=new Dog();
+		n[1]=new Cat();
+		n[2]=new Dog();
+		n[3]=new Cat();
+		for(Animal a:n) {
+			a.makeSound();
+			if(a instanceof Dog) {
+				 Dog map=(Dog)a;
+				 map.jump();
+			}
+		}
+
+	}
+
+}
