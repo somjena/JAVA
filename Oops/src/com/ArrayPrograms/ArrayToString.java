6a768bdb5903955a777c98ba0ac0d16beb2b9bc0
Soumitra jena <131749408+somjena@users.noreply.github.com>
2024-11-07 20:33:06 +0530

    ArrayToString Method

---
+++ Oops/src/com/ArrayPrograms/ArrayToString.java
@@ -0,0 +1,13 @@
+package com.ArrayPrograms;
+
+import java.util.Arrays;
+
+public class ArrayToString {
+
+	public static void main(String[] args) {
+		int arr[]= {10,20,30,40};
+		System.out.println(Arrays.toString(arr));
+
+	}
+
+}
