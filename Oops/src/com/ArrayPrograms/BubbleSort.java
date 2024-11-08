e3d853ccfcd1ad6f16343018f75abb324509f15f
Soumitra jena <131749408+somjena@users.noreply.github.com>
2024-11-08 23:33:47 +0530

    Bubble Sort Array

---
+++ Oops/src/com/ArrayPrograms/BubbleSort.java
@@ -0,0 +1,29 @@
+package com.ArrayPrograms;
+
+import java.util.Arrays;
+
+public class BubbleSort {
+
+	public static void main(String[] args) {
+		int a[]= {3,9,4,6,5,7,8};
+		System.out.println(Arrays.toString(a));
+		sort(a);
+		System.out.println(Arrays.toString(a));
+		
+
+	}
+	static void sort(int []a) {
+		for(int i =0;i<a.length-1;i++) {
+			for(int j=0;j<a.length-1-i;j++) {
+				if(a[j]>a[j+1]) {
+					int temp =a[j];
+					 a[j]=a[j+1];
+					 a[j+1]=temp;
+				}
+			}
+		}
+		
+	}
+	/*[3,9,4,6,5,7,8]*/
+
+}
