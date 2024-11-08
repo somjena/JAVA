56e0e6d33f80e9cae7554c2273295d7efa199942
Soumitra jena <131749408+somjena@users.noreply.github.com>
2024-11-08 21:33:48 +0530

    Second Small Number in array

---
+++ Oops/src/com/ArrayPrograms/SecondSmallNum.java
@@ -0,0 +1,26 @@
+package com.ArrayPrograms;
+
+public class SecondSmallNum {
+
+	public static void main(String[] args) {
+		
+		int[]a= {5,5,7,9,4,3,5};              /*{5,2,3,4,6,4,}----------->expected output :-4*/
+		int small=a[0];
+		int secondsmall=a[0];
+		
+		for(int i=1;i<a.length;i++) {
+			if(a[i]==small)continue;
+			if(a[i]<small) {
+				secondsmall=small;
+				small=a[i];
+			}else if(a[i]<secondsmall ||small==secondsmall) {
+				secondsmall=a[i];
+			}
+		
+			
+		}
+		System.out.println(secondsmall);
+
+	}
+
+}
