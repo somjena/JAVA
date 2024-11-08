f838a79eed5d8afc9acca646635dee743d829a0b
Soumitra jena <131749408+somjena@users.noreply.github.com>
2024-11-08 22:47:38 +0530

    Highest Number

---
+++ Oops/src/com/ArrayPrograms/HighestNumber.java
@@ -0,0 +1,20 @@
+package com.ArrayPrograms;
+
+public class HighestNumber {
+
+	public static void main(String[] args) {
+		int []a= {5,6,7,5,1,9,3};
+		int highest =a[0];
+		
+		for(int i =1;i<a.length;i++) {
+			if(a[i]>highest) {
+				highest=a[i];
+			}
+			
+		}
+		System.out.println(highest);
+		
+
+	}
+
+}
