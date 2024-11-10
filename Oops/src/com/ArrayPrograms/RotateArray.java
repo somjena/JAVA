ae38c18879608ee44a78b222c694060a5185e1f8
Soumitra jena <131749408+somjena@users.noreply.github.com>
2024-11-10 19:20:51 +0530

    rotate as per the reqirement

--- Oops/src/com/ArrayPrograms/RotateArray.java
+++ Oops/src/com/ArrayPrograms/RotateArray.java
@@ -5,9 +5,19 @@ import java.util.Arrays;
 public class RotateArray {
 
 	public static void main(String[] args) {
-		int []arr= {1,2,3,4,5,6,7,8,9};
-		rotate(arr);
-		System.out.println(Arrays.toString(arr));	
+		int []arr= {1,2,3,4,5};
+		/*rotate(arr);
+		System.out.println(Arrays.toString(arr));
+		rotate(arr);*/
+			
+		for(int i=0;i<2;i++) {
+			rotate(arr);
+			System.out.println(Arrays.toString(arr));
+		}
+			
+		
+		
+		//System.out.println(Arrays.toString(arr));	
 	}
 	public static void rotate(int[]a) {
 		int len =a.length;
