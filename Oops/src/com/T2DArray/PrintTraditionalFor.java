1db02021b6512b3b9af32034e79878b11c7a8e33
Soumitra jena <131749408+somjena@users.noreply.github.com>
2024-11-13 14:34:29 +0530

    Print 2d Array With Traditional For Loop

---
+++ Oops/src/com/T2DArray/PrintTraditionalFor.java
@@ -0,0 +1,21 @@
+package com.T2DArray;
+
+public class PrintTraditionalFor {
+
+	public static void main(String[] args) {
+		int [][]a= {
+
+				{1,2,3},
+				{4,5,6},
+				{7,8,9}
+		};
+		for(int i=0;i<a.length;i++) {
+			for(int j=0;j<a.length;j++) {
+				System.out.print(a[i][j] +"\t");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
