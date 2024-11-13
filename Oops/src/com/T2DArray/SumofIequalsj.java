6407e459a1c7772cd9c0599d4bf7e3f615b10728
Soumitra jena <131749408+somjena@users.noreply.github.com>
2024-11-13 14:34:50 +0530

    Sum of the Value when i==j

---
+++ Oops/src/com/T2DArray/SumofIequalsj.java
@@ -0,0 +1,23 @@
+package com.T2DArray;
+
+public class SumofIequalsj {
+
+	public static void main(String[] args) {
+		int Sum=0;
+		int [][]a= {
+
+				{1,2,3},
+				{4,5,6},
+				{7,8,9}
+		};
+		
+		for(int i=0;i<a.length;i++) {
+			for(int j=0;j<a.length;j++) {
+				if(i==j)Sum+=a[i][j];
+			}
+		}
+		System.out.println(Sum);
+
+	}
+
+}
