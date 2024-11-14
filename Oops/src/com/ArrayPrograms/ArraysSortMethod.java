1a9c3879c42e2ef1565e67933297218d0b886f7e
Soumitra jena <131749408+somjena@users.noreply.github.com>
2024-11-14 13:23:22 +0530

    Array Class Sort Method

---
+++ Oops/src/com/ArrayPrograms/ArraysSortMethod.java
@@ -0,0 +1,15 @@
+package com.ArrayPrograms;
+
+import java.util.Arrays;
+
+public class ArraysSortMethod {
+
+	public static void main(String[] args) {
+	
+		int []a= {2,3,8,9,4,7,6};
+		Arrays.sort(a);
+		System.out.println(Arrays.toString(a));
+
+	}
+
+}
