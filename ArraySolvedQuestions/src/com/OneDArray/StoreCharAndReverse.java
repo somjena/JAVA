b56016d81f9d6f2dfec91850810c29908cb5c7d7
Soumitra jena <131749408+somjena@users.noreply.github.com>
2024-11-15 13:23:25 +0530

    store the character in array and Print in reverse order

---
+++ ArraySolvedQuestions/src/com/OneDArray/StoreCharAndReverse.java
@@ -0,0 +1,13 @@
+package com.OneDArray;
+
+public class StoreCharAndReverse {
+
+	public static void main(String[] args) {
+		char []c= {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o'};
+		
+		for(int i=c.length-1;i>=0;i--) {
+			System.out.println(c[i]);
+		}
+	}
+
+}
