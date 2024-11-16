1162b577627654881efcb13f25236bc6f861a0c9
Soumitra jena <131749408+somjena@users.noreply.github.com>
2024-11-16 11:42:14 +0530

    Second Last Element of Array

---
+++ ArraySolvedQuestions/src/com/OneDArray/PrintSecondLastElement.java
@@ -0,0 +1,14 @@
+package com.OneDArray;
+
+public class PrintSecondLastElement {
+
+	public static void main(String[] args) {
+		int num[]= {1,2,3,4,5,6};
+		for(int i=0;i<num.length;i++) {
+			if(num[i]==num.length-1)
+				System.out.println(num[i]);
+		}
+
+	}
+
+}
