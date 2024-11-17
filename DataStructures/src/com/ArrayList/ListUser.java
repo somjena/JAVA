499187f1d996f6b5701ae2c3b8b4d6944e209c79
Soumitra jena <131749408+somjena@users.noreply.github.com>
2024-11-17 19:15:50 +0530

    User Impl

---
+++ DataStructures/src/com/ArrayList/ListUser.java
@@ -0,0 +1,42 @@
+package com.ArrayList;
+
+public class ListUser {
+
+	public static void main(String[] args) {
+		ArrayList a = new ArrayList();
+		System.out.println(a.Add("Soumitra Jena"));
+		System.out.println(a.Add("Amit Jena"));
+		System.out.println(a.Add("Ramakanta Jena"));
+		System.out.println(a.Add("Laxmipriya jena"));
+		System.out.println(a.Add("Subhashree jena"));
+		System.out.println(a.Add("Subhashree jena"));
+		System.out.println(a.Add("Subhashree jena"));
+		
+		System.out.println(a.Add("Subhashree jena"));
+		System.out.println(a.Add("Subhashree jena"));
+		System.out.println(a.Add("Subhashree jena"));
+		System.out.println(a.Add("Subhashree jena"));
+		System.out.println(a.Add("Subhashree jena"));
+		System.out.println(a.Add("Subhashree jena"));
+		System.out.println(a.Add("Soumitra Jena"));
+		System.out.println(a.Add("Amit Jena"));
+		System.out.println(a.Add("Ramakanta Jena"));
+		System.out.println(a.Add("Laxmipriya jena"));
+		System.out.println(a.Add("Subhashree jena"));
+		System.out.println(a.Add("Subhashree jena"));
+		System.out.println(a.Add("Subhashree jena"));
+		
+		System.out.println(a.Add("Subhashree jena"));
+		System.out.println(a.Add("Subhashree jena"));
+		System.out.println(a.Add("Subhashree jena"));
+		System.out.println(a.Add("Subhashree jena"));
+		System.out.println(a.Add("Subhashree jena"));
+		System.out.println(a.Add("Subhashree jena"));
+		
+		
+
+
+
+	}
+
+}
