bfa6eb09b49c32a49c45f437b81eff63c27772af
Soumitra jena <131749408+somjena@users.noreply.github.com>
2024-11-17 20:20:14 +0530

    Square of Each Element

---
+++ ArraySolvedQuestions/src/com/OneDArray/SquareOfEachElement.java
@@ -0,0 +1,16 @@
+package com.OneDArray;
+//Q19
+public class SquareOfEachElement {
+
+	public static void main(String[] args) {
+		int num[]= {1,2,3,4,5};
+		
+		for(int i=0;i<num.length;i++) {
+			int Square =num[i]*num[i];
+			System.out.print(Square+" ");
+		}
+		
+
+	}
+
+}
