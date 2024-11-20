bdae1c131c5924492c7c23cae662090ae9a7dbbd
Soumitra jena <131749408+somjena@users.noreply.github.com>
2024-11-20 10:50:37 +0530

    Creating the Methods to Add the Data into the Node

---
+++ DataStructures/src/com/LinkedList/SinglyLinkedList/LinkedList.java
@@ -0,0 +1,37 @@
+package com.LinkedList.SinglyLinkedList;
+
+public class LinkedList {
+	private Node first;
+	private int count;
+	
+	
+	public String Add(Object o) {
+		if(first==null) {
+			first=new Node(o);
+			count++;
+			return "SuccessFully Added  "+o+" At pos :- "+1;
+		}
+		Node curr=first;
+		while(curr.next!=null) {
+			curr=curr.next;
+		}
+		curr.next=new Node(o);
+		count++;
+		return "SuccessFully Added to the "+count+" "+o;
+		
+	}
+	public int size() {
+		return count;
+	}
+	public Object get(int index) {
+		if(index<=-1||index>=count) {
+			throw new IndexOutOfBoundsException();
+		}
+		Node curr =first;
+		for(int i=1;i<index;i++) {
+			curr=curr.next;
+		}
+		return curr.o;
+	}
+
+}
