721eef1d45ccc5acf909cdbcefac94ee5f7b9449
Soumitra jena <131749408+somjena@users.noreply.github.com>
2024-11-20 10:51:42 +0530

    Node Implementing Class

---
+++ DataStructures/src/com/LinkedList/SinglyLinkedList/NodeImpl.java
@@ -0,0 +1,28 @@
+package com.LinkedList.SinglyLinkedList;
+
+public class NodeImpl {
+
+	public static void main(String[] args) {
+		/*Node first =null;
+		first =new Node(10);
+		first.next=new Node(20);
+		first.next.next=new Node(30);
+		first.next.next.next=new Node(40);
+		
+		
+		
+		Node curr=first;
+		
+		while(curr!=null) {
+			System.out.println(curr.o);
+			curr=curr.next;
+		}
+		
+*/
+		LinkedList l = new LinkedList();
+		System.out.println(l.Add(10));
+		System.out.println(l.size());
+		System.out.println(l.get(0));
+	}
+
+}
