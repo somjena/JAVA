8b1540f32afa1bbefb6f8fd4264de0f9f01ddc62
Soumitra jena <131749408+somjena@users.noreply.github.com>
2024-11-20 21:38:29 +0530

    Created the Node Implementation Class to use the Methods

---
+++ DataStructures/src/com/LinkedList/DoublyLinkedList/DoubleLinkedListImpl.java
@@ -0,0 +1,16 @@
+package com.LinkedList.DoublyLinkedList;
+
+public class DoubleLinkedListImpl {
+
+	public static void main(String[] args) {
+		
+		LinkedList l= new LinkedList();
+		l.Add(10);
+		l.Add(20);
+		l.Add(30);
+		l.Add(40);
+		l.Add(50);
+		
+	}
+
+}
