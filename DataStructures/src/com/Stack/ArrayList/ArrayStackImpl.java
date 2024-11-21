19a7b0d763f2531628e35a6d8c7d8ceabdf252e1
Soumitra jena <131749408+somjena@users.noreply.github.com>
2024-11-21 15:13:19 +0530

    Impliment complete

--- DataStructures/src/com/Stack/ArrayList/ArrayStackImpl.java
+++ DataStructures/src/com/Stack/ArrayList/ArrayStackImpl.java
@@ -7,6 +7,9 @@ public class ArrayStackImpl {
 		s.push(10);
 		s.push(20);
 		s.push(30);
+		
+		System.out.println(s.pop());
+		System.out.println(s.pop());
 		System.out.println(s.peek());
 		System.out.println(s.size());
 
