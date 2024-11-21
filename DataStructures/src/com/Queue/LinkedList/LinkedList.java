bb08ce6ff4f7b93792e54f20048d7e194dc63ae1
Soumitra jena <131749408+somjena@users.noreply.github.com>
2024-11-21 13:16:25 +0530

    Created the Peek and poll method

--- DataStructures/src/com/Queue/LinkedList/LinkedList.java
+++ DataStructures/src/com/Queue/LinkedList/LinkedList.java
@@ -11,6 +11,7 @@ public class LinkedList {
 		if(first==null) {
 			first=new Node(ele);
 			count++;
+			return;
 		}
 		Node curr= first;
 		while(curr.next!=null) {
@@ -20,11 +21,40 @@ public class LinkedList {
 		count++;
 	}
 	
+	public void get() {
+		Node curr=first;
+		for(int i=0;i<Size();i++) {
+			System.out.println(curr.ele);
+			curr=curr.next;
+		}
+	}
+	
 
 	
 	
 	public int Size() {
 		return count;
 	}
+	
+	public Object peek() {
+		if(first==null) {
+			throw new IndexOutOfBoundsException();
+		}
+	return first.ele;
+	}
+	
+	
+	public Object poll() {
+		if(first==null) {
+			throw new IndexOutOfBoundsException();
+		}
+		Node temp =first;
+		
+		first=first.next;
+		count--;
+		return temp.ele;
+		
+	}
+	
 
 }
