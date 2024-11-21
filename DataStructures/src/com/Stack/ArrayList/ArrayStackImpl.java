b382be09ab81c189ed07198cb548e6c11b22df66
Soumitra jena <131749408+somjena@users.noreply.github.com>
2024-11-21 14:48:39 +0530

    ArrayStack Implementation

---
+++ DataStructures/src/com/Stack/ArrayList/ArrayStackImpl.java
@@ -0,0 +1,15 @@
+package com.Stack.ArrayList;
+
+public class ArrayStackImpl {
+
+	public static void main(String[] args) {
+		Stack s = new Stack();
+		s.push(10);
+		s.push(20);
+		s.push(30);
+		System.out.println(s.peek());
+		System.out.println(s.size());
+
+	}
+
+}
