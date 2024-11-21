5f73999f6a1918d0396b7a09a729e657b903054a
Soumitra jena <131749408+somjena@users.noreply.github.com>
2024-11-21 13:15:59 +0530

    Queue implimentation Using the LinkedList

---
+++ DataStructures/src/com/Queue/LinkedList/QueueLinkedList.java
@@ -0,0 +1,17 @@
+package com.Queue.LinkedList;
+
+public class QueueLinkedList {
+
+	public static void main(String[] args) {
+		LinkedList l = new LinkedList();
+		l.Add(10);
+		l.Add(20);
+		//System.out.println(l.Size());
+		//System.out.println(l.peek());
+		l.poll();
+		//System.out.println(l.Size());
+		l.get();
+
+	}
+
+}
