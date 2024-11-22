b912be47466055fc8d1b4331710b72c9c173bdd5
Soumitra jena <131749408+somjena@users.noreply.github.com>
2024-11-22 11:26:58 +0530

    Creating the methods for Stack Operations

---
+++ DataStructures/src/com/Stack/LinkedList/LinkedList.java
@@ -0,0 +1,22 @@
+package com.Stack.LinkedList;
+
+public class LinkedList {
+	private Node first;
+	private int count;
+	
+	
+	
+	public void push(Object o) {
+		if(first==null) {
+			first=new Node(o);
+			count++;
+			return;
+		}
+		Node temp=new Node(o);
+		first=new Node(o);
+		first.next=temp;
+		count++;
+		
+	}
+
+}
