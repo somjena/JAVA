a8bbd4c8658c00298c7f8b0b5289a5dda771752b
Soumitra jena <131749408+somjena@users.noreply.github.com>
2024-11-22 11:30:14 +0530

    Created the Stack User

---
+++ DataStructures/src/com/Stack/LinkedList/StackImplementation.java
@@ -0,0 +1,16 @@
+package com.Stack.LinkedList;
+
+public class StackImplementation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		LinkedList l =new LinkedList();
+		l.push(10);
+		l.push(20);
+		l.push(30);
+		l.push(30);
+		System.out.println(l.Size());
+
+	}
+
+}
