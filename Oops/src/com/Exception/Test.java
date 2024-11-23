854f1cecde32dde0fd963b8ed3bc35bc85f6a436
Soumitra jena <131749408+somjena@users.noreply.github.com>
2024-11-23 22:13:54 +0530

    Arithematic exveption

---
+++ Oops/src/com/Exception/Test.java
@@ -0,0 +1,22 @@
+package com.Exception;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println(10%0.0);
+		System.out.println(10/0.0);
+		
+		//System.out.println(10/0);// To Handle this exception we use try catch
+		
+		try {
+			System.out.println(10/0);
+		} catch (Exception e) {
+			
+			System.out.println("We can't Divide Any Number with Zero");
+		}
+
+	}
+
+}
