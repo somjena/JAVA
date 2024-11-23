07fef25ccec09c19bb3e294e50ded4bb65aca4f2
Soumitra jena <131749408+somjena@users.noreply.github.com>
2024-11-24 00:01:27 +0530

    correct the Syntax

--- Oops/src/com/Exception/FileException.java
+++ Oops/src/com/Exception/FileException.java
@@ -5,25 +5,25 @@ import java.io.FileReader;
 
 public class FileException {
 
-	public static void main(String[] args) {
-		System.out.println("main Executed");
-		try {
-			m1();
-		} catch (Exception e) {
-			// TODO: handle exception
-		}
-	}
+    public static void main(String[] args) {
+        System.out.println("main Executed");
+        try {
+            m1();
+        } catch (FileNotFoundException e) {
+            // Handle the exception
+            System.out.println("FileNotFoundException occurred: " + e.getMessage());
+        }
+    }
 
-	private static void m1() throws FileNotFoundException{
-		System.out.println("M1 Executed");
-		m2();
-		
-	}
+    private static void m1() throws FileNotFoundException {
+        System.out.println("m1 Executed");
+        m2();
+    }
 
-	private static void m2() throws FileNotFoundException {
-		System.out.println("m2 Executed");
-		FileReader r = new FileReader("E:\JSPYDER\JAVA\basics\Sum.java");
-		
-	}
-
+    private static void m2() throws FileNotFoundException {
+        System.out.println("m2 Executed");
+        
+        FileReader r = new FileReader("E:\\JSPYDER\\JAVA\\basics\\Sum.java");
+        System.out.println("File opened successfully.");
+    }
 }
