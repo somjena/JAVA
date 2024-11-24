2346aea1d7054a50b53cc65ed9da6caf405f5529
Soumitra jena <131749408+somjena@users.noreply.github.com>
2024-11-24 23:35:24 +0530

    Convert String to CharArray without builtin

---
+++ StringSolvedQuestions/src/com/String/ConcertStrintoCharArray.java
@@ -0,0 +1,15 @@
+package com.String;
+
+public class ConcertStrintoCharArray {
+
+	public static void main(String[] args) {
+		String s1="Hello World";
+		
+		for(int i=0;i<s1.length();i++) {
+			char c = s1.charAt(i);
+		}
+		
+
+	}
+
+}
