ea370bf9371f661b5ee8b25fca584c297e81850e
Soumitra jena <131749408+somjena@users.noreply.github.com>
2024-11-24 23:34:31 +0530

    Conver the Numbers According to requirement

---
+++ ArraySolvedQuestions/src/com/OneDArray/ConvertAccording.java
@@ -0,0 +1,24 @@
+package com.OneDArray;
+//Q.24
+public class ConvertAccording {
+
+	public static void main(String[] args) {
+		int []a= {0,2,-1,0,2,-1,3};
+		
+		for(int i=0;i<a.length;i++) {
+			if(a[i]==0){
+				a[i]=-1;
+				}
+			else if(a[i]==-1) {
+				a[i]=0;
+			}
+				
+		}
+		for(int n:a) {
+			System.out.println(n);
+		}
+
+
+	}
+
+}
