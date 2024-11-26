6ec96e7d425d0e1326af775742944262f0527d61
Soumitra jena <131749408+somjena@users.noreply.github.com>
2024-11-26 21:23:56 +0530

    Created the Hashset Class for the Object type and add the methods

---
+++ DataStructures/src/com/Hashset/ObjectType/Hashset.java
@@ -0,0 +1,47 @@
+package com.Hashset.ObjectType;
+
+public class Hashset {
+	private Node[]a = new Node[10];
+	private int count=0;
+	
+	
+	
+	public boolean add(Object key) {
+		int index=key.hashCode();
+		index=Math.abs(index);
+	
+	if(a[index]==null) {
+		a[index]=new Node(key,null);
+		count++;
+		return true;
+		
+	}
+	Node curr=a[index];
+	Node prev = null;
+	while(curr!=null) {
+		if(key.equals(curr.key))return false;
+		prev=curr;
+		curr=curr.next;
+	}
+	prev.next=new Node(key,null);
+	count++;
+	return true;
+}
+	public int size() {
+		return count;
+	}
+	
+	public void traverse() {
+		for(int i=0;i<a.length;i++) {
+			Node curr=a[i];
+			while(curr!=null) {
+				System.out.println(curr.key+" ");
+				curr=curr.next;
+			}
+		}
+		System.out.println();
+	}
+	public boolean isEmpty() {
+		return count==0;
+	}
+}
