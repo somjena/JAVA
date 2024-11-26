6fe789517d63957227d993fb70e40114d8a9e2e7
Soumitra jena <131749408+somjena@users.noreply.github.com>
2024-11-26 20:44:10 +0530

    Creating the User Class that implement the Hashset

---
+++ DataStructures/src/com/Hashset/Integer/User.java
@@ -0,0 +1,20 @@
+package com.Hashset.Integer;
+
+public class User {
+
+	public static void main(String[] args) {
+		Hashset h = new Hashset();
+		System.out.println(h.isEmpty());
+		h.Add(10);
+		h.Add(10);
+		h.Add(20);
+		h.Add(30);
+		h.Add(40);
+		
+		h.traverse();
+		
+		
+
+	}
+
+}
