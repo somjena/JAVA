6e4969a4cfbbf631a0b82dc65631a93444d07a93
Soumitra jena <131749408+somjena@users.noreply.github.com>
2024-11-28 15:15:31 +0530

    Creating the main Method or the implemented class

---
+++ DataStructures/src/com/Hashset/ObjectType/Car/CarRegister.java
@@ -0,0 +1,27 @@
+package com.Hashset.ObjectType.Car;
+
+import com.Hashset.ObjectType.Hashset;
+
+public class CarRegister {
+
+	public static void main(String[] args) {
+		
+		Car c1=new Car("Verna",201245,1);
+		Car c2=new Car("Verna",201245,1);
+		System.out.println(c1.hashCode());
+		System.out.println(c2.hashCode());
+		System.out.println(c1.equals(c2));
+		
+		
+		
+		Hashset h = new Hashset();
+		h.add(c1);
+		h.add(c2);
+		h.traverse();
+		
+
+
+
+	}
+
+}
