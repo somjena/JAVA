f39e4b2b1c8d7e2f3f10cd81493610f0eb5e3cec
Soumitra jena <131749408+somjena@users.noreply.github.com>
2024-12-01 11:24:54 +0530

    Created the ArrayList to Store the Int Tpe of Data Only

---
+++ DataStructures/src/com/ArrayList/Integer/IntArrayList.java
@@ -0,0 +1,29 @@
+package com.ArrayList.Integer;
+
+public class IntArrayList {
+	private int[]a;
+	private int pos;
+	
+	
+	public IntArrayList(){
+		a=new int [5];
+		pos=0;
+	}
+	
+	
+	public boolean Add(int n) {
+		if (pos>=a.length)increase();
+		a[pos]=n;
+		pos++;
+		return true;
+	}
+
+
+	public void increase() {
+		int[]temp = new int [a.length+3];
+		System.arraycopy(a, 0, temp, 0, a.length);
+		a=temp;
+		
+	}
+
+}
