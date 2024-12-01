31d1ffa5ecad63da2da9c24e4aad05f20a95882b
Soumitra jena <131749408+somjena@users.noreply.github.com>
2024-12-01 11:27:32 +0530

    ArrayList Implemented class

---
+++ DataStructures/src/com/ArrayList/Integer/StoreInt.java
@@ -0,0 +1,14 @@
+package com.ArrayList.Integer;
+
+public class StoreInt {
+
+	public static void main(String[] args) {
+		IntArrayList i =new IntArrayList();
+		System.out.println(i.Add(10));
+		System.out.println(i.Add(10));
+		System.out.println(i.Add(10));
+		System.out.println(i.Add(10));
+
+	}
+
+}
