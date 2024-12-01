8c9b479750bbaf4c6885f1bc5c3e2989ff12b810
Soumitra jena <131749408+somjena@users.noreply.github.com>
2024-12-01 11:37:43 +0530

    Implement get

--- DataStructures/src/com/ArrayList/Integer/StoreInt.java
+++ DataStructures/src/com/ArrayList/Integer/StoreInt.java
@@ -5,11 +5,12 @@ public class StoreInt {
 	public static void main(String[] args) {
 		IntArrayList i =new IntArrayList();
 		System.out.println(i.Add(10));
-		System.out.println(i.Add(10));
-		System.out.println(i.Add(10));
-		System.out.println(i.Add(10));
+		System.out.println(i.Add(20));
+		System.out.println(i.Add(30));
+		System.out.println(i.Add(40));
 		System.out.println(i.isEmpty());
 		System.out.println(i.Size());
+		System.out.println(i.get(3));
 
 	}
 
