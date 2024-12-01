4694eaa77519f2272c9d7af1acd892dd6e9ec51e
Soumitra jena <131749408+somjena@users.noreply.github.com>
2024-12-01 11:27:46 +0530

    Add the is Empty Method

--- DataStructures/src/com/ArrayList/Integer/IntArrayList.java
+++ DataStructures/src/com/ArrayList/Integer/IntArrayList.java
@@ -9,21 +9,21 @@ public class IntArrayList {
 		a=new int [5];
 		pos=0;
 	}
-	
-	
 	public boolean Add(int n) {
 		if (pos>=a.length)increase();
 		a[pos]=n;
 		pos++;
 		return true;
 	}
-
-
 	public void increase() {
 		int[]temp = new int [a.length+3];
 		System.arraycopy(a, 0, temp, 0, a.length);
 		a=temp;
 		
+	}
+	
+	public boolean isEmpty() {
+		return pos==0;
 	}
 
 }
