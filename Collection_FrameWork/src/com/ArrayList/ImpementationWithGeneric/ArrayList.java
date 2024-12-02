a3fd4f3beec72a0b24055e69d1c1b97b1c060d4a
Soumitra jena <131749408+somjena@users.noreply.github.com>
2024-12-02 21:42:48 +0530

    Creating the Arraylist to Store the Generic Type

---
+++ Collection_FrameWork/src/com/ArrayList/ImpementationWithGeneric/ArrayList.java
@@ -0,0 +1,29 @@
+package com.ArrayList.ImpementationWithGeneric;
+
+public class ArrayList<E> {
+	private Object []a;
+	private int pos;
+	
+	
+	public ArrayList() {
+		a=new Object[5];
+		pos=0;
+	}
+	
+	
+	public boolean add(E e) {
+		if (pos>=a.length)increase();
+		a[pos]=e;
+		pos++;
+		return true;
+	}
+
+	public void increase() {
+		Object []temp=new Object[a.length+3];
+		System.arraycopy(a, 0, temp, 0, a.length);
+		a=temp;
+		
+	}
+	
+
+}
