fa66bfd2806f83a37d52211076d06e96bd962061
Soumitra jena <131749408+somjena@users.noreply.github.com>
2024-12-02 19:51:38 +0530

    Store the Type of Integer Data in the Existing Collection

---
+++ Collection_FrameWork/src/com/ArrayList/Numbers.java
@@ -0,0 +1,20 @@
+package com.ArrayList;
+
+import java.util.ArrayList;
+
+public class Numbers {
+
+	public static void main(String[] args) {
+		ArrayList<Integer> c = new ArrayList<Integer>();
+		c.add(10);
+		c.add(20);
+		c.add(30);
+		System.out.println(c.add(40));
+		System.out.println(c.get(0));
+		System.out.println(c.iterator());
+		
+		
+
+	}
+
+}
