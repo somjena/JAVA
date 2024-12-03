94cd4d7b7101c9dc8dcc69c1d8fee897e98da268
Soumitra jena <131749408+somjena@users.noreply.github.com>
2024-12-03 20:49:41 +0530

    Implemented with Generic

---
+++ Collection_FrameWork/src/com/LinkedList/ImpementationWithGeneric/User.java
@@ -0,0 +1,12 @@
+package com.LinkedList.ImpementationWithGeneric;
+
+public class User {
+
+	public static void main(String[] args) {
+		LinkedList<Integer> i = new LinkedList<Integer>();
+		System.out.println(i.add(10));
+		
+
+	}
+
+}
