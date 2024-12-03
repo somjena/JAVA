90809d5b6f6b3e8e471aacb59d9bb32856904b9c
Soumitra jena <131749408+somjena@users.noreply.github.com>
2024-12-03 19:53:11 +0530

    Creating the Implementing User class

---
+++ Collection_FrameWork/src/com/ArrayList/ImpementationWithGeneric/User.java
@@ -0,0 +1,14 @@
+package com.ArrayList.ImpementationWithGeneric;
+
+public class User {
+
+	public static void main(String[] args) {
+		ArrayList<Integer> a = new ArrayList<Integer>();
+		a.add(20);
+		a.add(30);
+		int e=a.get(0);
+		System.out.println(e);
+
+	}
+
+}
