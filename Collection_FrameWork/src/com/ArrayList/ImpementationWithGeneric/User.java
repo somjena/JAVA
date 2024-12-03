cf696de70d512245e325ff91bef64ed1fc350428
Soumitra jena <131749408+somjena@users.noreply.github.com>
2024-12-03 20:31:47 +0530

    Corrected and implemented in between

--- Collection_FrameWork/src/com/ArrayList/ImpementationWithGeneric/User.java
+++ Collection_FrameWork/src/com/ArrayList/ImpementationWithGeneric/User.java
@@ -5,10 +5,12 @@ public class User {
 	public static void main(String[] args) {
 		ArrayList<Integer> a = new ArrayList<Integer>();
 		a.add(20);
-		a.add(30);
-		int e=a.get(0);
-		System.out.println(e);
-
+		a.add(40);
+		//int e=a.get(0);
+		//System.out.println(e);
+		a.add(30,1);
+		//System.out.println(a.get(1));
+		a.traverse();
 	}
 
 }
