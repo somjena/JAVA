11dea1ba46b514fa01710aa02c7ae04d56cc9801
Soumitra jena <131749408+somjena@users.noreply.github.com>
2024-12-04 12:40:36 +0530

    Implimented the size

--- Collection_FrameWork/src/com/LinkedList/ImpementationWithGeneric/User.java
+++ Collection_FrameWork/src/com/LinkedList/ImpementationWithGeneric/User.java
@@ -4,8 +4,10 @@ public class User {
 
 	public static void main(String[] args) {
 		LinkedList<Integer> i = new LinkedList<Integer>();
-		System.out.println(i.add(10));
-		
+		i.add(10);
+		i.add(20);
+		i.add(30);
+		System.out.println(i.size());
 
 	}
 
