26a92f9c67c5148b82e8e184b9e1315ebabc59fb
Soumitra jena <131749408+somjena@users.noreply.github.com>
2024-12-05 20:55:14 +0530

    Create the List to Store the Student

---
+++ Collection_FrameWork/src/com/List/Student/StudentList.java
@@ -0,0 +1,27 @@
+package com.List.Student;
+
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+
+
+public class StudentList {
+
+	public static void main(String[] args) {
+		Student s1 = new Student("Chef", 1, 43);
+		Student s2= new Student("Ballu", 2, 96);
+		Student s3 = new Student("Ankita",3,99);
+		
+		
+		List l =new LinkedList<Student>();
+		l.add(s1);
+		l.add(s2);
+		l.add(s3);
+		System.out.println(l);
+		Collections.sort(l, new NameComparator());
+		System.out.println(l);
+
+	}
+
+}
