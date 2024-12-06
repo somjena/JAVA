4394c1b32005bb626f51f3dcf0ddff222a007bd8
Soumitra jena <131749408+somjena@users.noreply.github.com>
2024-12-06 19:21:11 +0530

    Created the Employee Class

---
+++ Collection_FrameWork/src/com/Stack/Emp/Emp.java
@@ -0,0 +1,18 @@
+package com.Stack.Emp;
+
+public class Emp {
+	String name;
+	int id;
+	
+	
+	public Emp(String name,int id) {
+		this.name=name;
+		this.id=id;
+	}
+	
+	public String toString() {
+		return "name= "+name+" id= "+id;
+		
+	}
+
+}
