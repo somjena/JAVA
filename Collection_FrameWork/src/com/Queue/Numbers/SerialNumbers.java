fe54ba45e81269adb97c9a3fafaba3730ad0ed11
Soumitra jena <131749408+somjena@users.noreply.github.com>
2024-12-07 18:47:50 +0530

    Update

--- Collection_FrameWork/src/com/Queue/Numbers/SerialNumbers.java
+++ Collection_FrameWork/src/com/Queue/Numbers/SerialNumbers.java
@@ -3,19 +3,20 @@ package com.Queue.Numbers;
 import java.util.PriorityQueue;
 import java.util.Queue;
 
+import com.PriorityQueue.Numbers.EvenPrior;
+
 public class SerialNumbers {
 
 	public static void main(String[] args) {
-		Queue <Integer> q = new PriorityQueue<Integer>();
+		Queue <Integer> q = new PriorityQueue<Integer>(new EvenPrior());
 		q.add(1);
 		q.add(2);
 		q.add(3);
 		q.add(4);
+		
 		while(!q.isEmpty()) {
 			System.out.println(q.poll());
 		}
-		
-
 	}
 
 }
