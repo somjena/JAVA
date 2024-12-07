60321743c24ac1da4ad5691de7d3e674e41bbfd5
Soumitra jena <131749408+somjena@users.noreply.github.com>
2024-12-07 11:24:22 +0530

    Storing the Data  PriorityQueue

---
+++ Collection_FrameWork/src/com/Queue/Numbers/SerialNumbers.java
@@ -0,0 +1,21 @@
+package com.Queue.Numbers;
+
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class SerialNumbers {
+
+	public static void main(String[] args) {
+		Queue <Integer> q = new PriorityQueue<Integer>();
+		q.add(1);
+		q.add(2);
+		q.add(3);
+		q.add(4);
+		while(!q.isEmpty()) {
+			System.out.println(q.poll());
+		}
+		
+
+	}
+
+}
