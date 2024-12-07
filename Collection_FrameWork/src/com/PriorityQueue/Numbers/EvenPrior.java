76ab9260673c50083c1771a8c8a2add74b88669e
Soumitra jena <131749408+somjena@users.noreply.github.com>
2024-12-07 18:48:40 +0530

    Create a Comparator

---
+++ Collection_FrameWork/src/com/PriorityQueue/Numbers/EvenPrior.java
@@ -0,0 +1,14 @@
+package com.PriorityQueue.Numbers;
+
+import java.util.Comparator;
+
+public class EvenPrior implements Comparator<Integer> {
+
+    @Override
+    public int compare(Integer o1, Integer o2) {
+        // Prioritize even numbers
+        if (o1 % 2 == 0 && o2 % 2 != 0) return -1; // o1 is even, o2 is odd
+        if (o1 % 2 != 0 && o2 % 2 == 0) return 1;  // o1 is odd, o2 is even
+        return Integer.compare(o1, o2); // Ascending order for both even or both odd
+    }
+}
