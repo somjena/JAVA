eb26bc5fa959785132ceff3631b925a4326b4826
Soumitra jena <131749408+somjena@users.noreply.github.com>
2024-12-07 19:09:12 +0530

    Implement the isEmpty Method

--- Collection_FrameWork/src/com/PriorityQueue/Numbers/Num.java
+++ Collection_FrameWork/src/com/PriorityQueue/Numbers/Num.java
@@ -7,16 +7,15 @@ public class Num {
 
 	public static void main(String[] args) {
 		Queue<Integer> q = new PriorityQueue<Integer>(new DescComparator());
-		
-		
-		
+		System.out.println(q.isEmpty());//true
+		//Here we Insert the Value in the Mixed order  
 		q.add(10);
 		q.add(50);
 		q.add(40);
 		q.add(20);
 		q.add(30);
-		
-		
+		System.out.println(q.peek());
+		System.out.println(q.isEmpty());
 		while(!q.isEmpty()) {
 			System.out.println(q.poll()); 
 		}
