93e212a3a3b63f36567673a27dc951146e4b0f71
Soumitra jena <131749408+somjena@users.noreply.github.com>
2024-12-08 19:29:27 +0530

    Create the Class to Store the Employee

---
+++ Collection_FrameWork/src/com/PriorityQueue/Emp/StoreEmp.java
@@ -0,0 +1,31 @@
+package com.PriorityQueue.Emp;
+
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class StoreEmp {
+
+	public static void main(String[] args) {
+		Emp e1 = new Emp("Som",201);
+		Emp e2 = new Emp("Shrabana",202);
+		Emp e3 = new Emp("Ankita",203);
+		Emp e4 = new Emp("Bablu",204);
+		
+		
+		Queue <Emp>q = new PriorityQueue<Emp>(new IdComparator());
+		q.add(e1);
+		q.add(e2);
+		q.add(e3);
+		q.add(e4);
+		
+		
+		
+		
+		//System.out.println(q.peek());
+		while(!q.isEmpty()) {
+			System.out.println(q.poll());
+		}
+
+	}
+
+}
