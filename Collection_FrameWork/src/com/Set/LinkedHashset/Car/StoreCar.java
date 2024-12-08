d3fd1276c04afb2f04472ad51ba3c99f73ccf43f
Soumitra jena <131749408+somjena@users.noreply.github.com>
2024-12-09 01:07:41 +0530

    Created the Implemented Car Store Class

---
+++ Collection_FrameWork/src/com/Set/LinkedHashset/Car/StoreCar.java
@@ -0,0 +1,24 @@
+package com.Set.LinkedHashset.Car;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class StoreCar {
+
+	public static void main(String[] args) {
+		Car c1 = new Car("BMW", new Engine(1200));
+		Car c2 = new Car("BMW", new Engine(1200));
+		Car c3 = new Car("BMW", new Engine(1200));
+		Car c4 = new Car("BMW", new Engine(1200));
+		
+		
+		Set s = new LinkedHashSet();
+		s.add(c1);
+		s.add(c2);
+		s.add(c3);
+		s.add(c4);
+		System.out.println(s);
+
+	}
+
+}
