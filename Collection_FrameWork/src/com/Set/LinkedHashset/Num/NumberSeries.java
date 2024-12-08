94465e440837f97ea087dcb1925723e338f33b57
Soumitra jena <131749408+somjena@users.noreply.github.com>
2024-12-09 00:29:42 +0530

    Add the Numbers into the Linked Hashset

---
+++ Collection_FrameWork/src/com/Set/LinkedHashset/Num/NumberSeries.java
@@ -0,0 +1,23 @@
+package com.Set.LinkedHashset.Num;
+
+import java.util.Iterator;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class NumberSeries {
+
+	public static void main(String[] args) {
+		Set <Integer>s = new LinkedHashSet<Integer>();
+		s.add(5);
+		s.add(1);
+		s.add(2);
+		s.add(3);
+		s.add(4);
+		Iterator i = s.iterator();
+		while(i.hasNext()) {
+			System.out.println(i.next());
+		}
+
+	}
+
+}
