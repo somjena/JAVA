bf82e9153c5c98a87a5e217395f11d4ad80b09bf
Soumitra jena <131749408+somjena@users.noreply.github.com>
2024-12-08 19:42:53 +0530

    Create a List and Store Multiple Type of Data

---
+++ Collection_FrameWork/src/com/List/Object/StoreObjects.java
@@ -0,0 +1,21 @@
+package com.List.Object;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StoreObjects {
+
+	public static void main(String[] args) {
+		List l = new ArrayList();
+		l.add(10);
+		l.add(20);
+		l.add("a");
+		l.add(7.5);
+		l.add(201245789L);
+		System.out.println(l);
+		
+		
+
+	}
+
+}
