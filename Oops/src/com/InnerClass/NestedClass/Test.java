83e12763d9dcc83a80ae9305502398890a1d4d8e
Soumitra jena <131749408+somjena@users.noreply.github.com>
2024-12-11 15:16:54 +0530

    Inner Class Concept

---
+++ Oops/src/com/InnerClass/NestedClass/Test.java
@@ -0,0 +1,25 @@
+package com.InnerClass.NestedClass;
+
+class A{
+	
+	static int i =10;
+	static class B{
+		void m1() {
+			System.out.println("M1 in Inner B");
+			System.out.println(i);
+		}
+	}
+	
+}
+
+public class Test {
+
+	public static void main(String[] args) {
+		A.B b1 = new A.B() ;
+		b1.m1();
+		
+		
+
+	}
+
+}
