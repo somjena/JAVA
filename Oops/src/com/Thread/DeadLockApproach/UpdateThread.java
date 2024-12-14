5e4fe3af1e622c8e9cf5faacbb4cbbbd84800a27
Soumitra jena <131749408+somjena@users.noreply.github.com>
2024-12-14 20:27:58 +0530

    Update the run Method and Constructor

--- Oops/src/com/Thread/DeadLockApproach/UpdateThread.java
+++ Oops/src/com/Thread/DeadLockApproach/UpdateThread.java
@@ -1,5 +1,17 @@
 package com.Thread.DeadLockApproach;
 
-public class UpdateThread {
+public class UpdateThread implements Runnable{
+
+	int items;
+	public UpdateThread(int items) {
+		this.items=items;
+		new Thread(this).start();
+	}
+	@Override
+	public void run() {
+		System.out.println("Update thread Starts");
+		StockYard.getinstance().update(items);
+		
+	}
 
 }
