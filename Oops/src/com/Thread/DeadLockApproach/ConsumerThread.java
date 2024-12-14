93b5c2a26ff6fd6619dbc7b09f04a647dd319987
Soumitra jena <131749408+somjena@users.noreply.github.com>
2024-12-14 20:32:44 +0530

    Implement the Consume Thread

--- Oops/src/com/Thread/DeadLockApproach/ConsumerThread.java
+++ Oops/src/com/Thread/DeadLockApproach/ConsumerThread.java
@@ -1,5 +1,17 @@
 package com.Thread.DeadLockApproach;
 
-public class ConsumerThread {
+public class ConsumerThread implements Runnable{
+
+	int items;
+	public ConsumerThread(int items) {
+		this.items=items;
+		new Thread(this).start();
+	}
+	@Override
+	public void run() {
+		System.out.println("Consumer Thread Started");
+		StockYard.getinstance().consume(items);
+		
+	}
 
 }
