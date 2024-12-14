53be9a01b80362533ed8e8d817daabb3b727526d
Soumitra jena <131749408+somjena@users.noreply.github.com>
2024-12-14 20:46:29 +0530

    Make All the Method Syncronized

--- Oops/src/com/Thread/DeadLockApproach/StockYard.java
+++ Oops/src/com/Thread/DeadLockApproach/StockYard.java
@@ -1,16 +1,12 @@
 package com.Thread.DeadLockApproach;
 
 public class StockYard {
-	int items;
+	private int items;
 	private static StockYard obj=null;
 	
 	private StockYard() {
 		
 	}
-	
-	
-	
-	
 	public static StockYard getinstance() {
 		if(obj==null)obj=new StockYard();
 		return obj;
@@ -18,7 +14,7 @@ public class StockYard {
 		
 	}
 	
-	public void update(int items) {
+	synchronized public void update(int items) {
 		this.items+=items;
 		System.out.println("items Updated to "+items);
 		
@@ -26,10 +22,12 @@ public class StockYard {
 	
 	
 	
-	public void consume(int items) {
+	synchronized public void consume(int items) {
 		System.out.println("Required Items"+items);
 		while(this.items<items) {
 			System.out.println("Waiting for Update");
+			try{Thread.sleep(500);}catch(Exception e) {}
+			
 		}
 		
 		this.items-=items;
