6c2ba054be2fbab319a55ef4f3aa342e51fb7a02
Soumitra jena <131749408+somjena@users.noreply.github.com>
2024-12-14 20:46:38 +0530

    Update

--- Oops/src/com/Thread/DeadLockApproach/SalesBranch1.java
+++ Oops/src/com/Thread/DeadLockApproach/SalesBranch1.java
@@ -4,6 +4,10 @@ public class SalesBranch1 {
 
 	public static void main(String[] args) {
 		
+		
+		new ConsumerThread(30);
+		try{Thread.sleep(500);}catch(Exception e) {}
+		new UpdateThread(30);
 
 	}
 
