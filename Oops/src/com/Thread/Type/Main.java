2cf7516c03dbc6ecbba4e37290f8cfb8de91f0d8
Soumitra jena <131749408+somjena@users.noreply.github.com>
2024-12-15 19:06:51 +0530

    calling the setdeamon method and make arg as false

---
+++ Oops/src/com/Thread/Type/Main.java
@@ -0,0 +1,21 @@
+package com.Thread.Type;
+
+public class Main {
+	public static void main(String[] args) {
+		 Thread t1 = new Thread(new Alpha());
+		t1.setDaemon(false);// if We make it false then it will be deamon thread
+		 
+		 t1.start();
+		 
+		 for(char c='A';c<='Z';c++) {
+			 System.out.println(c);
+			 try {
+				Thread.sleep(500);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		 }
+	}
+
+}
