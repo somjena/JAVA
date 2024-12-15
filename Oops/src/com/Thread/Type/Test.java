a4de4e8c8ec38b336d26a192dffa43f959845d25
Soumitra jena <131749408+somjena@users.noreply.github.com>
2024-12-15 19:27:18 +0530

    Update

--- Oops/src/com/Thread/Type/Test.java
+++ Oops/src/com/Thread/Type/Test.java
@@ -8,6 +8,8 @@ public class Test {
 		System.out.println(t1.getName());
 		t1.setName("NumberThread");
 		System.out.println(t1.getName());
+		System.out.println("Thread id :- "+t1.getId());
+		System.out.println("Priority "+t1.getPriority());
 		
 
 	}
