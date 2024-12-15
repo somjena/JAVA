e8be515e8f8b639fcfdec9401bf16f1b8ce679dd
Soumitra jena <131749408+somjena@users.noreply.github.com>
2024-12-15 19:08:57 +0530

    Now we make the Alpha class as Nondeamon

--- Oops/src/com/Thread/Type/Main.java
+++ Oops/src/com/Thread/Type/Main.java
@@ -3,8 +3,8 @@ package com.Thread.Type;
 public class Main {
 	public static void main(String[] args) {
 		 Thread t1 = new Thread(new Alpha());
-		t1.setDaemon(false);// if We make it false then it will be deamon thread
-		 
+		//t1.setDaemon(false);// if We make it false then it will be deamon thread
+		 t1.setDaemon(true);
 		 t1.start();
 		 
 		 for(char c='A';c<='Z';c++) {
