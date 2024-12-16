2b23c1dde562b6f1a93d7e37b92ddacacc9c8b27
Soumitra jena <131749408+somjena@users.noreply.github.com>
2024-12-16 21:13:49 +0530

    Created the Comparator that will Store the Data According to The Descending Order

---
+++ Collection_FrameWork/src/com/Map/SortedMap/Car/CarComp.java
@@ -0,0 +1,13 @@
+package com.Map.SortedMap.Car;
+
+import java.util.Comparator;
+
+public class CarComp implements Comparator<Integer>{
+
+	@Override
+	public int compare(Integer o1, Integer o2) {
+		// TODO Auto-generated method stub
+		return o2-o1;
+	}
+
+}
