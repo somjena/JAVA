f301104d85ee4fa5b3e1b756b7bc6637af02d42e
Soumitra jena <131749408+somjena@users.noreply.github.com>
2024-12-16 20:58:05 +0530

    Store The Data According to the Descending order

---
+++ Collection_FrameWork/src/com/Map/SortedMap/Employee/DescComp.java
@@ -0,0 +1,15 @@
+package com.Map.SortedMap.Employee;
+
+import java.util.Comparator;
+
+public class DescComp implements Comparator<Integer>{
+	
+		@Override
+		public int compare(Integer o1, Integer o2) {
+			// TODO Auto-generated method stub
+			return o2-o1;
+		}
+		
+	}
+
+
