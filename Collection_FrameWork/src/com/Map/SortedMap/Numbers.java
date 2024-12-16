c16312193f84034784a6a79093a307cba03ef099
Soumitra jena <131749408+somjena@users.noreply.github.com>
2024-12-16 20:36:58 +0530

    Numbers Map

---
+++ Collection_FrameWork/src/com/Map/SortedMap/Numbers.java
@@ -0,0 +1,17 @@
+package com.Map.SortedMap;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+public class Numbers {
+
+	public static void main(String[] args) {
+		Map <Integer,Integer>m = new TreeMap<Integer,Integer>();
+		m.put(1, 10);
+		m.put(2,20);
+		m.put(3, 30);
+		m.put(3, 40);
+		int n=m.get(3);
+		System.out.println(n);
+	}
+}
