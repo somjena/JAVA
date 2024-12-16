f52d5038ee625959a33d569314c8f65e74b0b349
Soumitra jena <131749408+somjena@users.noreply.github.com>
2024-12-16 20:37:25 +0530

    update

--- Collection_FrameWork/src/com/Map/SortedMap/Numbers.java
+++ Collection_FrameWork/src/com/Map/SortedMap/Numbers.java
@@ -10,7 +10,7 @@ public class Numbers {
 		m.put(1, 10);
 		m.put(2,20);
 		m.put(3, 30);
-		m.put(3, 40);
+		m.put(3, 40);// Instead of Creating New It Modify the Given Key's Value
 		int n=m.get(3);
 		System.out.println(n);
 	}
