8376f92f4671483a1663f001687add4e9cfe7370
Soumitra jena <131749408+somjena@users.noreply.github.com>
2024-12-17 14:44:07 +0530

    Update

--- Collection_FrameWork/src/com/Map/HashMap/Mobile/StorePhone.java
+++ Collection_FrameWork/src/com/Map/HashMap/Mobile/StorePhone.java
@@ -3,19 +3,27 @@ package com.Map.HashMap.Mobile;
 import java.util.HashMap;
 import java.util.Map;
 
+
+
 public class StorePhone {
 
 	public static void main(String[] args) {
+		Phone p1 = new Phone(128, 12, "Vivo");
+		p1.insertSim(new Sim("Jio"));
+		Phone p2 = new Phone(128, 12, "Oppo");
+		p2.insertSim(new Sim("Airtel"));
+		
+		
+		
 	Map<Integer, Phone> p = new HashMap<Integer, Phone>();
 	
 	;
-	p.put(1, new Phone(128, 8, "OPPO"));
-	p.put(1, new Phone(128, 8, "OPPO"));
-	p.put(1, new Phone(128, 8, "OPPO"));
-	p.put(1, new Phone(128, 8, "OPPO"));
-	for(Map.Entry<Integer, Phone> entry:p.entrySet()) {}
+	p.put(1, p1);
+	p.put(2, p2);
 	
-
+	for(Map.Entry<Integer, Phone> entry:p.entrySet()) {
+		System.out.println("Key: " + entry.getKey() + ", Value: " + entry.getValue());
+	}
 	}
 
 }
