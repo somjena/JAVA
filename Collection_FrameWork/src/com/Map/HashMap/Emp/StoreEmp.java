a52ae56775363f4edcb0c6041be407f9dcf4436b
Soumitra jena <131749408+somjena@users.noreply.github.com>
2024-12-17 14:51:09 +0530

    Created the Map to Store Emp

---
+++ Collection_FrameWork/src/com/Map/HashMap/Emp/StoreEmp.java
@@ -0,0 +1,14 @@
+package com.Map.HashMap.Emp;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class StoreEmp {
+
+	public static void main(String[] args) {
+		Map<Integer, Emp> e1 = new HashMap<Integer, Emp>();
+		e1.put(1, new Emp("Ankitaa", 1));
+
+	}
+
+}
