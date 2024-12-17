de72c53a84c4ac77d524a1e2ca404e0faf208ec7
Soumitra jena <131749408+somjena@users.noreply.github.com>
2024-12-17 13:52:12 +0530

    Update the Generic

--- Collection_FrameWork/src/com/Map/SortedMap/Car/StoreCar.java
+++ Collection_FrameWork/src/com/Map/SortedMap/Car/StoreCar.java
@@ -8,13 +8,15 @@ import java.util.TreeMap;
 public class StoreCar {
 
 	public static void main(String[] args) {
-		Map<Integer,Car> m = new TreeMap<Integer,Car>();
+		
+		
+		Map<Integer,Car> m = new TreeMap<Integer,Car>(new CarComp());
 		m.put(1011, new Car("BMW", new Engine(1200)));
 		m.put(1012, new Car("AUDI",new Engine(1400)));
-		m.put(1013, new Car("BENZ",new Engine(1400)));
-		m.put(1014, new Car("MERCEDES",new Engine(1400)));
-		Set s = m.keySet();
-		Iterator i=s.iterator();
+		m.put(1013, new Car("BENZ",new Engine(1800)));
+		m.put(1014, new Car("MERCEDES",new Engine(1600)));
+		Set<Integer> s = m.keySet();
+		Iterator<Integer> i=s.iterator();
 		while(i.hasNext()) {
 			System.out.println(m.get(i.next()));
 		}
