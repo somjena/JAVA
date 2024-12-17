e5a7dbcae30285653a0a921fbd94cd5cb53cf8b4
Soumitra jena <131749408+somjena@users.noreply.github.com>
2024-12-17 14:31:07 +0530

    Create the Class Phone

---
+++ Collection_FrameWork/src/com/Map/HashMap/Mobile/Phone.java
@@ -0,0 +1,37 @@
+package com.Map.HashMap.Mobile;
+
+public class Phone {
+	int rom;
+	int ram;
+	String color;
+	Sim s;
+	
+	
+	
+	public Phone(int rom, int ram, String color) {
+		this.rom = rom;
+		this.ram = ram;
+		this.color = color;
+	}
+	
+	
+	public Sim insertSim(Sim s) {
+		return this.s=s;
+		
+	}
+
+
+	@Override
+	public String toString() {
+		return "Phone [rom=" + rom + ", ram=" + ram + ", color=" + color + ", s=" + s + "]";
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
