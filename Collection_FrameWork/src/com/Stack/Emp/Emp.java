d07f364c9863ecb71d55d75270a2ddd60ab2a0c9
Soumitra jena <131749408+somjena@users.noreply.github.com>
2024-12-18 21:23:55 +0530

    Update

--- Collection_FrameWork/src/com/Stack/Emp/Emp.java
+++ Collection_FrameWork/src/com/Stack/Emp/Emp.java
@@ -3,15 +3,15 @@ package com.Stack.Emp;
 public class Emp {
 	String name;
 	int id;
-	
-	
 	public Emp(String name,int id) {
 		this.name=name;
 		this.id=id;
 	}
 	
+	
+	
 	public String toString() {
-		return "name= "+name+" id= "+id;
+		return "name = "+name+" id= "+id;
 		
 	}
 
