46ef5d630d4d05b62466af3fc13cb82999e23b79
Soumitra jena <131749408+somjena@users.noreply.github.com>
2024-12-18 21:22:14 +0530

    Update

--- Collection_FrameWork/src/com/Map/Programs/Romanizer.java
+++ Collection_FrameWork/src/com/Map/Programs/Romanizer.java
@@ -5,14 +5,35 @@ import java.util.Map;
 
 public class Romanizer {
 	public static void main(String[] args) {
-		System.out.println(convert(20));
+		System.out.println(convert(2041));
 	}
 
 	private static String convert(int num) {
 		Map<Integer, String> m = new LinkedHashMap<Integer, String>();
 		
 		String res = "";
-		return null;
+		m.put(1, "I");
+		m.put(4, "IV");
+		m.put(5, "V");
+		m.put(9, "IX");
+		m.put(10, "X");
+		m.put(40, "XL");
+		m.put(50, "L");
+		m.put(90, "XC");
+		m.put(100, "C");
+		m.put(400, "CD");
+		m.put(500, "D");
+		m.put(900, "CM");
+		
+		int []n = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
+		for(int value :n) {
+			while(num>=value) {
+				res+=m.get(value);
+				num-=value;
+			}
+		}
+		
+		return res;
 	}
 
 }
