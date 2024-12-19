b171858c3ba429dfef37f95425ff894cd209bd87
Soumitra jena <131749408+somjena@users.noreply.github.com>
2024-12-19 21:41:36 +0530

    Implements some Methods

--- Oops/src/com/FileHandling/CreateFile.java
+++ Oops/src/com/FileHandling/CreateFile.java
@@ -8,7 +8,17 @@ public class CreateFile {
 	public static void main(String[] args) throws IOException {
 		File f = new File("E:\\Soumitra\\java.txt");
 		if(f.createNewFile())System.out.println(" new File Created at - "+f.getAbsolutePath());
+		System.out.println(f.canExecute());
+		System.out.println(f.canWrite());
+		//f.delete();
 
+		File f2 = new File("E:\\Soumitra\\javadev.txt");
+		System.out.println(f.renameTo(f2));
+		f2.delete();
+		System.out.println(f.canExecute());
+		System.out.println(f.canWrite());
+		
 	}
 
 }
+
