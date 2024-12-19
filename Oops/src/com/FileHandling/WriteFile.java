7de71204337a042e79f532e2c8c95525dd1add8d
Soumitra jena <131749408+somjena@users.noreply.github.com>
2024-12-19 21:56:50 +0530

    Write in the File

---
+++ Oops/src/com/FileHandling/WriteFile.java
@@ -0,0 +1,22 @@
+package com.FileHandling;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class WriteFile {
+
+	public static void main(String[] args) throws IOException {
+		File f = new File("E:\\Soumitra\\java.txt");
+		
+		try(FileWriter fw = new FileWriter(f,true)){
+		if(f.canWrite()) {
+			fw.write("Jspiders"+"\n"+"Pyspiders");
+		}else {
+			System.out.println("Can Write into this file");
+		}
+		fw.write("hello");
+		}
+	}
+
+}
