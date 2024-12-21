5173acd7035852b7f2a31e6f84ad56a532ae7bfb
Soumitra jena <131749408+somjena@users.noreply.github.com>
2024-12-21 11:24:53 +0530

    Square of a Number in a List

---
+++ Oops/src/com/Lambda_Expression/Function/SquareofNum.java
@@ -0,0 +1,21 @@
+package com.Lambda_Expression.Function;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
+
+public class SquareofNum {
+
+	public static void main(String[] args) {
+	List<Integer> l =	Arrays.asList(1,2,3,4,5,6,7,8,9);
+	Function<Integer, Integer> f = n->n*n;
+	
+	
+	
+	for(int n :l) {
+		System.out.println(f.apply(n));
+		
+	}
+	}
+
+}
