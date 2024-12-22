5f4390fc8687db0e8d5e7a6f20f48b60f808b553
Soumitra jena <131749408+somjena@users.noreply.github.com>
2024-12-22 11:25:40 +0530

    Update

--- Oops/src/com/Lambda_Expression/Function/Programs/CheckStringSize.java
+++ Oops/src/com/Lambda_Expression/Function/Programs/CheckStringSize.java
@@ -1,13 +1,19 @@
 package com.Lambda_Expression.Function.Programs;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.function.Function;
 
 public class CheckStringSize {
 
 	public static void main(String[] args) {
+		List<String> l  = Arrays.asList("Som","Bablu","pintu","rintu");
 		
 		Function<String, Integer> getSize = f ->f.length();
-		System.out.println(getSize.apply("rintu"));
+		
+		for(String s :l) {
+			System.out.println("______"+s+"______"+ " size is "+getSize.apply(s));
+		}
 
 	}
 
