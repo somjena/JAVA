ad986a1a261d5ec476f518d419150f78364d835a
Soumitra jena <131749408+somjena@users.noreply.github.com>
2024-12-23 12:53:56 +0530

    Create The Employee Class

---
+++ Oops/src/com/Lambda_Expression/Function/Emp/Emp.java
@@ -0,0 +1,26 @@
+package com.Lambda_Expression.Function.Emp;
+
+public class Emp {
+	String name;
+	int id;
+	int age;
+	String state;
+	
+	public Emp(String name, int id, int age, String state) {
+		this.name = name;
+		this.id = id;
+		this.age = age;
+		this.state = state;
+	}
+
+	@Override
+	public String toString() {
+		return "Emp [name=" + name + ", id=" + id + ", age=" + age + ", state=" + state + "]";
+	}
+	
+	
+	
+	
+	
+
+}
