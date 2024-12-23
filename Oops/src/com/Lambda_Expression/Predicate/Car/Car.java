6bd39e585a54f08f89a6a4f7939bb80a72c5d40b
Soumitra jena <131749408+somjena@users.noreply.github.com>
2024-12-23 15:46:18 +0530

    Create the Car Class

---
+++ Oops/src/com/Lambda_Expression/Predicate/Car/Car.java
@@ -0,0 +1,16 @@
+package com.Lambda_Expression.Predicate.Car;
+
+public class Car {
+	int cc;
+	public Car(int cc) {
+		this.cc = cc;
+	}
+	@Override
+	public String toString() {
+		return  cc + " CC";
+	}
+	
+	
+	
+
+}
