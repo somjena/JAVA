6bbbb48cdaab24bf3553ffd1a48ad88ec0bccbd4
Soumitra jena <131749408+somjena@users.noreply.github.com>
2024-12-23 16:00:13 +0530

    Update the Sequence

--- Oops/src/com/Lambda_Expression/Predicate/Car/CarDriver.java
+++ Oops/src/com/Lambda_Expression/Predicate/Car/CarDriver.java
@@ -8,9 +8,9 @@ public class CarDriver {
 
 	public static void main(String[] args) {
 		Car c1 = new Car("BMW", 1,new Engine(2200));
-		Car c2 = new Car("AUDI", 1,new Engine(1800));
-		Car c3 = new Car("MERCEDES", 1,new Engine(2100));
-		Car c4 = new Car("DUCATI", 1,new Engine(4000));
+		Car c2 = new Car("AUDI", 2,new Engine(1800));
+		Car c3 = new Car("MERCEDES", 3,new Engine(2100));
+		Car c4 = new Car("DUCATI", 4,new Engine(4000));
 		
 		List<Car> list = Arrays.asList(c1,c2,c3,c4);
 		Predicate<Car> p = c->c.e.cc>=2200;
