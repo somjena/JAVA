48a17f6d901dd440f67f3ed926adca8793d49dee
Soumitra jena <131749408+somjena@users.noreply.github.com>
2024-12-23 15:58:37 +0530

    Update the Car Driver Lambda Expressions

---
+++ Oops/src/com/Lambda_Expression/Predicate/Car/CarDriver.java
@@ -0,0 +1,27 @@
+package com.Lambda_Expression.Predicate.Car;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class CarDriver {
+
+	public static void main(String[] args) {
+		Car c1 = new Car("BMW", 1,new Engine(2200));
+		Car c2 = new Car("AUDI", 1,new Engine(1800));
+		Car c3 = new Car("MERCEDES", 1,new Engine(2100));
+		Car c4 = new Car("DUCATI", 1,new Engine(4000));
+		
+		List<Car> list = Arrays.asList(c1,c2,c3,c4);
+		Predicate<Car> p = c->c.e.cc>=2200;
+		
+		
+		for(Car c:list) {
+			if(p.test(c)) {
+				System.out.println(c);
+			}
+		}
+
+	}
+
+}
