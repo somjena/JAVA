4162cf0f9bb651232ac1ac01ac968f058b11cf16
Soumitra jena <131749408+somjena@users.noreply.github.com>
2024-12-23 15:48:25 +0530

    Creating the Engine of the Class

---
+++ Oops/src/com/Lambda_Expression/Predicate/Car/Engine.java
@@ -0,0 +1,13 @@
+package com.Lambda_Expression.Predicate.Car;
+
+public class Engine {
+	int cc;
+	public Engine(int cc) {
+		this.cc=cc;
+		
+	}
+	public String toString() {
+		return cc+" CC";
+	}
+
+}
