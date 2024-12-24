10a05204dfd1a59286bd2bcc98639adef24e305d
Soumitra jena <131749408+somjena@users.noreply.github.com>
2024-12-24 06:28:50 +0530

    Store the Numbers into Queue

---
+++ Collection_FrameWork/src/com/Dequeue/Numbers.java
@@ -0,0 +1,23 @@
+package com.Dequeue;
+
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+public class Numbers {
+
+	public static void main(String[] args) {
+		Queue<Integer> q = new ArrayDeque<Integer>();
+		q.add(10);
+		q.add(20);
+		q.add(50);
+		q.add(40);
+		q.add(30);
+		System.out.println(q.peek());
+		
+		
+		while(!q.isEmpty()) {
+			System.out.println(q.poll());
+		}
+	}
+
+}
