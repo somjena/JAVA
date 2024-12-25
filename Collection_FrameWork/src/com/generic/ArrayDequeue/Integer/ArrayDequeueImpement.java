2828086f1d5c3365854dd9a6e3da305aae24b95a
Soumitra jena <131749408+somjena@users.noreply.github.com>
2024-12-25 16:09:07 +0530

    Implement Class

---
+++ Collection_FrameWork/src/com/generic/ArrayDequeue/Integer/ArrayDequeueImpement.java
@@ -0,0 +1,25 @@
+package com.generic.ArrayDequeue.Integer;
+
+public class ArrayDequeueImpement {
+
+	public static void main(String[] args) {
+		ArrayDequeue<Integer> ad = new ArrayDequeue<Integer>();
+		ad.add(10);
+		ad.add(20);
+		ad.add(30);
+		ad.add(40);
+		ad.add(50);
+		ad.add(60);
+		ad.add(70);
+		ad.add(80);
+		ad.add(90);
+		ad.add(100);
+		ad.add(110);
+		ad.add(220);
+		ad.add(130);
+		ad.add(140);
+		System.out.println(ad.isEmpty());
+		System.out.println(ad.Size());
+	}
+
+}
