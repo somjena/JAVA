e03de5af66fd2118092f3bb514d0db7aa7b5e0be
Soumitra jena <131749408+somjena@users.noreply.github.com>
2024-12-27 11:31:17 +0530

    Create the Node

---
+++ DataStructures/src/com/ArrayDequeue/LinkedList/Node.java
@@ -0,0 +1,15 @@
+package com.ArrayDequeue.LinkedList;
+
+public class Node {
+	private Node next;
+	private int count;
+	public Node(Node next) {
+		this.next = next;
+		count=0;;
+	}
+	
+	
+	
+	
+
+}
