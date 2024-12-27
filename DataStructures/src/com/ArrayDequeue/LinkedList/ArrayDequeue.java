611eeed016550f7e3508528167b28bf950e15d11
Soumitra jena <131749408+somjena@users.noreply.github.com>
2024-12-27 11:39:49 +0530

    push Method Added

--- DataStructures/src/com/ArrayDequeue/LinkedList/ArrayDequeue.java
+++ DataStructures/src/com/ArrayDequeue/LinkedList/ArrayDequeue.java
@@ -1,8 +1,25 @@
 package com.ArrayDequeue.LinkedList;
 
+
 public class ArrayDequeue {
 
 	private Node first;
+	private int count;
+	
+	
+	public void push(Object ele) {
+		if(first==null) {
+			first=new Node(ele);
+			count++;
+			return;
+			
+		}
+		Node temp=first;
+		first=new Node(ele);
+		first.next=temp;
+		count++;
+
+	}
 
 	
 
