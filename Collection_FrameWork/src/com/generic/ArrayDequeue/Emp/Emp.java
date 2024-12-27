6ea8bfdaa836d86f3e779afcd9ea79b7fa1c5206
Soumitra jena <131749408+somjena@users.noreply.github.com>
2024-12-27 11:24:16 +0530

    Create the Employee Class

---
+++ Collection_FrameWork/src/com/generic/ArrayDequeue/Emp/Emp.java
@@ -0,0 +1,26 @@
+package com.generic.ArrayDequeue.Emp;
+
+public class Emp {
+	String name;
+	int id;
+	int age;
+	double sal;
+	
+	public Emp(String name, int id, int age, double sal) {
+		this.name = name;
+		this.id = id;
+		this.age = age;
+		this.sal = sal;
+	}
+	
+
+	@Override
+	public String toString() {
+		return "Emp [name=" + name + ", id=" + id + ", age=" + age + ", sal=" + sal + "]";
+	}
+	
+	
+	
+	
+
+}
