76a66e38690b4cd99de1507044821a7df085c21b
Soumitra jena <131749408+somjena@users.noreply.github.com>
2024-12-28 12:58:25 +0530

    Implement Class

---
+++ DataStructures/src/com/ArrayDequeue/LinkedList/ArrayDequeueImpl.java
@@ -0,0 +1,17 @@
+package com.ArrayDequeue.LinkedList;
+
+public class ArrayDequeueImpl {
+
+	public static void main(String[] args) {
+		ArrayDequeue a = new ArrayDequeue();
+		a.push(10);
+		a.push(20);
+		a.push(30);
+		a.push(40);
+		//a.popFirst();
+		//System.out.println(a.size());
+		System.out.println(a.popFirst());
+
+	}
+
+}
