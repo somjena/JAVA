19574ecfbdf11547e57d23e70ce1c18aa98d4643
Soumitra jena <131749408+somjena@users.noreply.github.com>
2024-12-29 13:46:42 +0530

    Created the User Class

---
+++ InterviewPrep/src/com/DataStructure/LinkedList/User.java
@@ -0,0 +1,14 @@
+package com.DataStructure.LinkedList;
+
+public class User {
+
+	public static void main(String[] args) {
+		LinkedList<Integer> l = new LinkedList<Integer>();
+		System.out.println(l.add(10));
+		System.out.println(l.add(10));
+		System.out.println(l.add(10));
+		System.out.println(l.add(10));
+
+	}
+
+}
