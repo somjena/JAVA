99d3b5ca87a28af32c92597f796642c5377be1ef
Soumitra jena <131749408+somjena@users.noreply.github.com>
2024-12-30 12:12:25 +0530

    Creating the ArrayList Class

---
+++ InterviewPrep/src/com/DataStructure/ArrayList/ArrayList.java
@@ -0,0 +1,17 @@
+package com.DataStructure.ArrayList;
+
+public class ArrayList <E>{
+	private Object[] a;
+	private int pos;
+	
+	
+	public ArrayList() {
+		a=new Object [10];
+		pos=0;
+	}
+	
+	
+	
+	//add Method
+	
+}
