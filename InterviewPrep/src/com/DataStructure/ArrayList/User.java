ae595a0f27a0ab59bc2edda981295fb97e7c3c27
Soumitra jena <131749408+somjena@users.noreply.github.com>
2024-12-30 13:05:21 +0530

    Use the methods

--- InterviewPrep/src/com/DataStructure/ArrayList/User.java
+++ InterviewPrep/src/com/DataStructure/ArrayList/User.java
@@ -3,6 +3,9 @@ package com.DataStructure.ArrayList;
 public class User {
 	public static void main(String[] args) {
 		ArrayList<Integer> i = new ArrayList<Integer>();
+		System.out.println(i.add(10));
+		System.out.println(i.add(20));
+		
 	}
 
 }
