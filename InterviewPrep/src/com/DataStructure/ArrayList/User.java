b8a098cb92459fc48970294afd898ff871a49430
Soumitra jena <131749408+somjena@users.noreply.github.com>
2024-12-31 12:57:05 +0530

    add Overload Method Tested Successfully

--- InterviewPrep/src/com/DataStructure/ArrayList/User.java
+++ InterviewPrep/src/com/DataStructure/ArrayList/User.java
@@ -7,12 +7,14 @@ public class User {
 		System.out.println(i.add(20));
 		//int res = i.get(0);
 		//System.out.println(res);
+		System.out.println(i.add(30, 1));
 	
 		for (int a = 0; a <i.size(); a++) {
 		    if (!i.isEmpty()) {
 		        System.out.println(i.get(a));
 		    }
 		}
+		
 
 	}
 
