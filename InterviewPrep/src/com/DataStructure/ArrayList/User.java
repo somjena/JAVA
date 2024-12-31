987f80f45c4bdd9b3b56409d8acfc2e56b6de9cf
Soumitra jena <131749408+somjena@users.noreply.github.com>
2025-01-01 00:48:44 +0530

    Update the User Class

--- InterviewPrep/src/com/DataStructure/ArrayList/User.java
+++ InterviewPrep/src/com/DataStructure/ArrayList/User.java
@@ -3,18 +3,19 @@ package com.DataStructure.ArrayList;
 public class User {
 	public static void main(String[] args) {
 		ArrayList<Integer> i = new ArrayList<Integer>();
-		System.out.println(i.add(10));
-		System.out.println(i.add(20));
+		i.add(10);
+		i.add(20);
 		//int res = i.get(0);
 		//System.out.println(res);
-		System.out.println(i.add(30, 1));
-		i.Remove(1);
+		i.add(30, 1);
+		//i.Remove(1);
+		i.display();
 	
-		for (int a = 0; a <i.size(); a++) {
+		/*for (int a = 0; a <i.size(); a++) {
 		    if (!i.isEmpty()) {
 		        System.out.println(i.get(a));
 		    }
-		}
+		}*/
 		
 
 	}
