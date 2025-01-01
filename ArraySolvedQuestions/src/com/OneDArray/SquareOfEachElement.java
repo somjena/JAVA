fade3c0932e19f6cd23976759ca7632c451c7285
Soumitra jena <131749408+somjena@users.noreply.github.com>
2025-01-01 17:58:38 +0530

    Square of Each Element

--- ArraySolvedQuestions/src/com/OneDArray/SquareOfEachElement.java
+++ ArraySolvedQuestions/src/com/OneDArray/SquareOfEachElement.java
@@ -5,8 +5,8 @@ public class SquareOfEachElement {
 	public static void main(String[] args) {
 		int num[]= {1,2,3,4,5};
 		
-		for(int i=0;i<num.length;i++) {
-			int Square =num[i]*num[i];
+		for(int i=0;i<num.length;i++) {//traverse the array from 0th index to the Last index
+			int Square =num[i]*num[i];//int Variable Which Store the Sqr of Each Element
 			System.out.print(Square+" ");
 		}
 		
