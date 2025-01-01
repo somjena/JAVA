b880f7286ed756335a482b17ff4364a7b1c2fa92
Soumitra jena <131749408+somjena@users.noreply.github.com>
2025-01-01 17:52:11 +0530

    prime Number in  Array

--- ArraySolvedQuestions/src/com/OneDArray/PrimeNumbersinArray.java
+++ ArraySolvedQuestions/src/com/OneDArray/PrimeNumbersinArray.java
@@ -1,13 +1,13 @@
 package com.OneDArray;
 
 public class PrimeNumbersinArray {
-
+//Prime Number In Array
 	public static void main(String[] args) {
 		int ar[]= {1,2,3,4,5,6};
 		
 		
 		for(int i =0;i<ar.length;i++) {
-			boolean Flag =true;
+			boolean Flag =true;//flag variable to mark
 			if(ar[i]==0||ar[i]==1)continue;
 			for(int j=2;j<=ar[i]/2;j++) {
 				if(ar[i]%j==0) {
