c8e8cb5f333382dc1f25723a8fbc4935116fb9e0
Soumitra jena <131749408+somjena@users.noreply.github.com>
2025-01-01 17:59:38 +0530

    Update

--- ArraySolvedQuestions/src/com/OneDArray/PrintOddElements.java
+++ ArraySolvedQuestions/src/com/OneDArray/PrintOddElements.java
@@ -5,7 +5,7 @@ public class PrintOddElements {
 	public static void main(String[] args) {
 		int []num= {1,2,3,4,5,6};
 		
-		for(int nums:num) {
+		for(int nums:num) {// for Each loop
 			if(nums%2!=0) {
 				System.out.println(nums);
 			}
