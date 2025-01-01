562ffac6bda89f54d5390c0e7d78a0ffe3b790e8
Soumitra jena <131749408+somjena@users.noreply.github.com>
2025-01-01 17:51:11 +0530

    odd index Value

--- ArraySolvedQuestions/src/com/OneDArray/OddIndexValue.java
+++ ArraySolvedQuestions/src/com/OneDArray/OddIndexValue.java
@@ -4,9 +4,9 @@ public class OddIndexValue {
 
 	public static void main(String[] args) {
 		int []a= {1,2,3,4,5,6};
-		for(int i =0;i<a.length;i++) {
+		for(int i =0;i<a.length;i++) {//it will run till the array index
 			if(i%2!=0)
-				System.out.print(a[i]+" ");
+				System.out.print(a[i]+" ");// Only Print the odd Value 
 		}
 
 	}
