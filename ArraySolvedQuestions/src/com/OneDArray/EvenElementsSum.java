d883bbd2eb9fc9de6806e8fb0f819383cb52ea1a
Soumitra jena <131749408+somjena@users.noreply.github.com>
2025-01-01 17:47:36 +0530

    update

--- ArraySolvedQuestions/src/com/OneDArray/EvenElementsSum.java
+++ ArraySolvedQuestions/src/com/OneDArray/EvenElementsSum.java
@@ -3,11 +3,11 @@ package com.OneDArray;
 public class EvenElementsSum {
 
 	public static void main(String[] args) {
-		int num[]= {1,2,3,4,5,6};
-		int Esum=0;
+		int num[]= {1,2,3,4,5,6};//int Type Array
+		int Esum=0; // Esum Variable to Store the Integer Value 
 		for(int i =0;i<num.length;i++) {
 			if(num[i]%2==0) {
-				 Esum+=num[i];
+				 Esum+=num[i];//Sum of Each The Element In the Arrray
 			}
 		}
 		System.out.println(Esum);
