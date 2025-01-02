739496fde36f818a81953e57b2767ccd83485406
Soumitra jena <131749408+somjena@users.noreply.github.com>
2025-01-02 21:43:42 +0530

    Remove

--- Oops/src/com/Abstraction/Vehicle/Vehicle.java
+++ Oops/src/com/Abstraction/Vehicle/Vehicle.java
@@ -6,46 +6,6 @@ abstract public class Vehicle {
 	 abstract void stop();
 
 }
-class BMW extends Vehicle{
-
-	@Override
-	void start() {
-		System.out.println("BMW Start");
-		
-	}
-
-	@Override
-	void acc() {
-		System.out.println("BMW Accelarate");
-		
-	}
-
-	@Override
-	void stop() {
-		System.out.println("BMW Stop");
-		
-	}
-	
-}
-class Benz extends Vehicle{
-
-	@Override
-	void start() {
-		System.out.println("BENZ Start");
-		
-	}
-
-	@Override
-	void acc() {
-		System.out.println("BENZ Accelarate");
-	}
-
-	@Override
-	void stop() {
-		System.out.println("BENZ Stop");
-	}
-	
-}
 
 class Driver{
 	public static void  Drive(Vehicle v) {
