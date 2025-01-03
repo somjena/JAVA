beca3a1b23ad093b8c7234a388ef7b3c01fe64c3
Soumitra jena <131749408+somjena@users.noreply.github.com>
2025-01-03 22:59:08 +0530

    Update the Reference

--- Oops/src/com/ArrayPrograms/CopyArray.java
+++ Oops/src/com/ArrayPrograms/CopyArray.java
@@ -5,7 +5,7 @@ public class CopyArray {
 	public static void main(String[] args) {
 		int []a= {10,20,30,40};// Declare an Array
 		int []b;
-		b=a; 
+		b=a; // copy the Reference
 		b[0]=50;
 		for(int ar:b) {
 			System.out.println(ar);
