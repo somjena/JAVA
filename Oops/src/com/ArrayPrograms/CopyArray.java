35d49008c33ff8933517b1ffc79b32f4860a5c9e
Soumitra jena <131749408+somjena@users.noreply.github.com>
2025-01-03 22:58:40 +0530

    copy array

--- Oops/src/com/ArrayPrograms/CopyArray.java
+++ Oops/src/com/ArrayPrograms/CopyArray.java
@@ -3,9 +3,9 @@ package com.ArrayPrograms;
 public class CopyArray {
 
 	public static void main(String[] args) {
-		int []a= {10,20,30,40};
+		int []a= {10,20,30,40};// Declare an Array
 		int []b;
-		b=a;
+		b=a; 
 		b[0]=50;
 		for(int ar:b) {
 			System.out.println(ar);
