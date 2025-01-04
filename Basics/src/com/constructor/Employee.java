3139ef8ac1448507055e5e6e25a72f0894647471
Soumitra jena <131749408+somjena@users.noreply.github.com>
2025-01-04 23:19:11 +0530

    Emp Update

--- Basics/src/com/constructor/Employee.java
+++ Basics/src/com/constructor/Employee.java
@@ -1,19 +1,5 @@
 package com.constructor;
-class RegisterEmp{
-	String name;
-	int age;
-	double height;
-	
-	public RegisterEmp(String name, int age, double height) {
-		
-		this.name = name;
-		this.age = age;
-		this.height = height;
-	}
-	
-	
-	
-}
+
 public class Employee {
 	public static void main(String[] args) {
 		RegisterEmp e1 = new RegisterEmp("Som", 22, 5.6);
