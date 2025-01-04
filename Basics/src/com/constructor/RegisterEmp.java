58003c5f2079f460ec941fcfe7dff5b4262027e0
Soumitra jena <131749408+somjena@users.noreply.github.com>
2025-01-04 23:18:59 +0530

    Register emp

---
+++ Basics/src/com/constructor/RegisterEmp.java
@@ -0,0 +1,17 @@
+package com.constructor;
+
+class RegisterEmp{
+	String name;
+	int age;
+	double height;
+	
+	public RegisterEmp(String name, int age, double height) {
+		
+		this.name = name;
+		this.age = age;
+		this.height = height;
+	}
+	
+	
+	
+}
