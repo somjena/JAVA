f277503636acc3c588ef5dae0754736524dc057d
Soumitra jena <131749408+somjena@users.noreply.github.com>
2025-01-07 23:04:04 +0530

    String to CharArray

--- StringSolvedQuestions/src/com/String/ConcertStrintoCharArray.java
+++ StringSolvedQuestions/src/com/String/ConcertStrintoCharArray.java
@@ -1,12 +1,17 @@
 package com.String;
 
+import java.util.Scanner;
+
 public class ConcertStrintoCharArray {
 
 	public static void main(String[] args) {
-		String s1="Hello World";
-		
-		for(int i=0;i<s1.length();i++) {
-			char c = s1.charAt(i);
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the String");
+		String sr = sc.nextLine();
+				
+		for(int i=0;i<sr.length();i++) {
+			char c = sr.charAt(i);
+			System.out.println(c);
 		}
 		
 
