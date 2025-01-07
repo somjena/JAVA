2f42e9fe20ddfcc31ff5ce037bb976807435c947
Soumitra jena <131749408+somjena@users.noreply.github.com>
2025-01-07 23:06:49 +0530

    Count Word Using Split Methd

--- StringSolvedQuestions/src/com/String/CountWords.java
+++ StringSolvedQuestions/src/com/String/CountWords.java
@@ -1,12 +1,13 @@
 package com.String;
 
+import java.util.Scanner;
+
 public class CountWords {
 
 	public static void main(String[] args) {
-		String s1 = "java is easy";
-	
-	
-
+		Scanner sc= new Scanner(System.in);
+		System.out.println("Enter the String");
+		String s1 = sc.nextLine();
 		String[] s2 = s1.split(" ");
 		System.out.println(s2.length);
 		
