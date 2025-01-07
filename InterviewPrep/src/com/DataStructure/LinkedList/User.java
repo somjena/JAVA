ef3f65a15434b0d97ccebc88fd73446e447179bd
Soumitra jena <131749408+somjena@users.noreply.github.com>
2025-01-07 23:15:22 +0530

    Employee Class Store

--- InterviewPrep/src/com/DataStructure/LinkedList/User.java
+++ InterviewPrep/src/com/DataStructure/LinkedList/User.java
@@ -10,6 +10,13 @@ public class User {
 		System.out.println(l.add(40));
 		System.out.println(l.size());
 		System.out.println(l.isEmpty());
+		//------------------------------------//
+		Emp e1 = new Emp("som", 22);
+		Emp e2 = new Emp("Rj",24);
+		LinkedList<Emp> elm = new LinkedList<Emp>();
+		elm.add(e1);
+		elm.add(e2);
+		System.out.println(elm.get(0));
 		
 		int res =l.get(3);
 		System.out.println(res);
