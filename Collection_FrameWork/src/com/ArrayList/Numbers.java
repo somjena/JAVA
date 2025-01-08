ff1f2b585d56a8cf947bf4a623ff8951b9507a78
Soumitra jena <131749408+somjena@users.noreply.github.com>
2025-01-08 23:21:27 +0530

    Update iterator

--- Collection_FrameWork/src/com/ArrayList/Numbers.java
+++ Collection_FrameWork/src/com/ArrayList/Numbers.java
@@ -1,6 +1,7 @@
 package com.ArrayList;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class Numbers {
 
@@ -10,10 +11,15 @@ public class Numbers {
 		c.add(20);
 		c.add(30);
 		//c.add("Som");
-		System.out.println(c.add(40));
+		/*System.out.println(c.add(40));
 		System.out.println(c.get(0));
 		c.remove(0);
-		System.out.println(c.get(0));
+		System.out.println(c.get(0));*/
+		
+		Iterator itr=c.iterator();
+		while(itr.hasNext()) {
+			System.out.println(itr.next());
+		}
 		
 		
 		
