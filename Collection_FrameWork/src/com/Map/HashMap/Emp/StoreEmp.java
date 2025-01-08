80f085f710dd4b4bcaa31eb7c34f0e8be4f17980
Soumitra jena <131749408+somjena@users.noreply.github.com>
2025-01-08 23:26:42 +0530

    Update

--- Collection_FrameWork/src/com/Map/HashMap/Emp/StoreEmp.java
+++ Collection_FrameWork/src/com/Map/HashMap/Emp/StoreEmp.java
@@ -8,6 +8,11 @@ public class StoreEmp {
 	public static void main(String[] args) {
 		Map<Integer, Emp> e1 = new HashMap<Integer, Emp>();
 		e1.put(1, new Emp("Ankitaa", 1));
+		e1.put(1, new Emp("Rintu", 1));
+		e1.put(1, new Emp("Ankitaa", 1));
+		e1.put(1, new Emp("Ankitaa", 1));
+	
+		
 
 	}
 
