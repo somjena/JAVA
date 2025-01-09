5b60b8e279feae5e35935cdb3b5251dd9c477562
Soumitra jena <131749408+somjena@users.noreply.github.com>
2025-01-09 20:29:40 +0530

    Add Method

--- DataStructures/src/com/BinarySearchTree/Number/BinarySearchTree.java
+++ DataStructures/src/com/BinarySearchTree/Number/BinarySearchTree.java
@@ -4,5 +4,26 @@ public class BinarySearchTree {
 	private Node root;
 	private int count;
 	
+	
+	
+	//add Method 
+	public boolean add(int key) {
+		if(root==null) {
+			root =new Node(key);
+			count++;
+			return true;
+		}
+		Node curr=root;
+		Node prev=null;
+		
+		while(curr!=null) {
+			if(key<curr.key) {
+				prev=curr;
+				curr=curr.left;
+			}
+			
+		}
+	}
+	
 
 }
