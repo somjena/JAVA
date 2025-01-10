930ec6b4e0f6783a3a94b7dd8ade3e66de533a59
Soumitra jena <131749408+somjena@users.noreply.github.com>
2025-01-10 23:50:35 +0530

    Update User

--- DataStructures/src/com/BinarySearchTree/Number/User.java
+++ DataStructures/src/com/BinarySearchTree/Number/User.java
@@ -3,7 +3,10 @@ package com.BinarySearchTree.Number;
 public class User {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		BinarySearchTree bst = new BinarySearchTree();
+		bst.add(1);
+		bst.add(2);
+		
 
 	}
 
