965b9da7fe54f0d578eba508199c32fc2c03242d
Soumitra jena <131749408+somjena@users.noreply.github.com>
2025-01-10 23:53:19 +0530

    Update the Node Class

--- DataStructures/src/com/BinarySearchTree/Number/Node.java
+++ DataStructures/src/com/BinarySearchTree/Number/Node.java
@@ -4,12 +4,13 @@ public class Node {
 	Node left;
 	Node right;
 	int key;
-	
+	//constructor to initialize object
 	public Node(Node left,Node right,int key) {
 		this.left=left;
 		this.right=right;
 		this.key=key;
 	}
+	//Constructor Overloading
 	public Node (int key) {
 		this.key=key;
 	}
