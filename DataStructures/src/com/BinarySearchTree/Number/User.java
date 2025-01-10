10d4043d1fc23d2130e83bec4670ea4b3285dc3c
Soumitra jena <131749408+somjena@users.noreply.github.com>
2025-01-10 23:54:06 +0530

    Update

--- DataStructures/src/com/BinarySearchTree/Number/User.java
+++ DataStructures/src/com/BinarySearchTree/Number/User.java
@@ -4,13 +4,12 @@ public class User {
 
 	public static void main(String[] args) {
 		BinarySearchTree bst = new BinarySearchTree();
+		System.out.println(bst.size());
+		System.out.println(bst.isEmpty());
 		bst.add(1);
 		bst.add(2);
 		System.out.println(bst.size());
 		System.out.println(bst.isEmpty());
-		
-		
-
 	}
 
 }
