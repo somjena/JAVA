9b67b5326fc7f8708a304c660fc826f5da52bbcb
Soumitra jena <131749408+somjena@users.noreply.github.com>
2025-01-12 21:27:20 +0530

    Remove

--- Basics/src/com/overloading/StudentDriver.java
+++ Basics/src/com/overloading/StudentDriver.java
@@ -1,32 +1,6 @@
 package com.overloading;
 import java.util.*;
-class Student {
-    private String name;
-    private int age;
-    private String grade;
-    
-   
-    public Student(String name, int age, String grade) {
-        this.name = name;
-        this.age = age;
-        this.grade = grade;
-    }
 
-   
-    public void displayDetails(String name) {// Overload Method with One Argument
-        System.out.println("Name: " + name);
-    }
-
-  
-    public void displayDetails(String name, int age) {//Overload Method with Two Argument
-        System.out.println("Name: " + name + ", Age: " + age);
-    }
-
-    
-    public void displayDetails(String name, int age, String grade) {//Overload Method With Three Argument
-        System.out.println("Name: " + name + ", Age: " + age + ", Grade: " + grade);
-    }
-}
 
 public class StudentDriver {
     public static void main(String[] args) {
