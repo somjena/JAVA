68a18c0b5f4dc72e9ea6ab18d496491f1660bda7
Soumitra jena <131749408+somjena@users.noreply.github.com>
2025-01-14 21:36:12 +0530

    Created the Employee Driver Class

---
+++ Oops/src/com/Encapsulation/Employee/EmpDriver.java
@@ -0,0 +1,11 @@
+package com.Encapsulation.Employee;
+
+public class EmpDriver {
+
+	public static void main(String[] args) {
+		Emp e1  =new Emp();
+		e1.setName("Soumitra");
+		System.out.println(e1.getName());
+	}
+
+}
