864373ff4f42f939dc9077d35e02c85259d86948
Soumitra jena <131749408+somjena@users.noreply.github.com>
2025-01-14 21:47:34 +0530

    Create the StudentDriver Class

---
+++ Oops/src/com/Encapsulation/Student/StudentDriver.java
@@ -0,0 +1,12 @@
+package com.Encapsulation.Student;
+
+public class StudentDriver {
+
+	public static void main(String[] args) {
+		
+		Student student = new Student("Alice", 201, "S101");
+
+        
+	}
+
+}
