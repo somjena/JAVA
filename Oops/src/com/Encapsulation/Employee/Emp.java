daf20c606d6929bca396d1a96fc2982fd73295f0
Soumitra jena <131749408+somjena@users.noreply.github.com>
2025-01-14 21:34:24 +0530

    Encapsulation

---
+++ Oops/src/com/Encapsulation/Employee/Emp.java
@@ -0,0 +1,26 @@
+package com.Encapsulation.Employee;
+
+public class Emp {
+	private String name;
+	private int id;
+	
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	
+
+}
