5a169707976d348aabd7da302451cfc22ff8d0e7
Soumitra jena <131749408+somjena@users.noreply.github.com>
2025-01-15 21:59:23 +0530

    Update excpt

--- Oops/src/com/Exception/AccountDriver.java
+++ Oops/src/com/Exception/AccountDriver.java
@@ -5,7 +5,7 @@ public class AccountDriver {
 	public static void main(String[] args) {
 		Account a = new Account(1000);
 		try {
-			a.withdraw(1000);
+			a.withdraw(1000);//when we use the withdraw method a checked exception occured
 		} catch (InsufficientBalException e) {
 			// TODO Auto-generated catch block
 			System.out.println(e.getMessage());
