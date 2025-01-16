7374d514f2c16c4e4715823812923a6e37070ce2
Soumitra jena <131749408+somjena@users.noreply.github.com>
2025-01-16 22:55:23 +0530

    update

--- Basics/src/com/constructor/Constructor.java
+++ Basics/src/com/constructor/Constructor.java
@@ -2,7 +2,7 @@ package com.constructor;
 
 public class Constructor {
 	public static void main(String[] args) {
-		A a1 = new A(10,20);
+		A a1 = new A(10,20);//initialize
 		System.out.println(a1.a);
 		System.out.println(a1.b);
 	}
