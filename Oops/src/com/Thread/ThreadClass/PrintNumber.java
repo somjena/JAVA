290917697d4dabc35c6a02f8e0b351a60a139447
Soumitra jena <131749408+somjena@users.noreply.github.com>
2025-01-18 18:31:58 +0530

    remove the Class

--- Oops/src/com/Thread/ThreadClass/PrintNumber.java
+++ Oops/src/com/Thread/ThreadClass/PrintNumber.java
@@ -15,15 +15,5 @@ public class PrintNumber {
 }
 
 
-class PrintNum extends Thread{
-	public void run() {
-		System.out.println("Created new Thread");
-		for(int i =1;i<=10;i++) {
-			System.out.println(i);
-			try{Thread.sleep(500);} catch(Exception e) {};
-		}
-		System.out.println();
-	}
-}
 
 
