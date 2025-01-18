6cd8fba2b82bfc2fde17dd7dc78631c898e12b7f
Soumitra jena <131749408+somjena@users.noreply.github.com>
2025-01-18 18:29:21 +0530

    rmv

--- Oops/src/com/Thread/ThreadClass/PrintAll.java
+++ Oops/src/com/Thread/ThreadClass/PrintAll.java
@@ -1,17 +1,6 @@
 package com.Thread.ThreadClass;
 
-class Alphabet extends Thread{
-	@Override
-	public void run() {
-		System.out.println("Alphabet Thread initialized");
-		for(int i = 1;i<=26;i++) {
-			System.out.println(i);
-			
-			try {Thread.sleep(500);} catch(Exception e) {}
-		}
-		System.out.println("Alphabet Thread End");
-	}
-}
+
 public class PrintAll {
 
 	public static void main(String[] args) {
