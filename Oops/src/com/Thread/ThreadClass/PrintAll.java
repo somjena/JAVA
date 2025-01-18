bcff0fd1ef73cb39c458d6c85d59c3fdff9874db
Soumitra jena <131749408+somjena@users.noreply.github.com>
2025-01-18 18:27:59 +0530

    remove class

--- Oops/src/com/Thread/ThreadClass/PrintAll.java
+++ Oops/src/com/Thread/ThreadClass/PrintAll.java
@@ -12,19 +12,6 @@ class Alphabet extends Thread{
 		System.out.println("Alphabet Thread End");
 	}
 }
-class Number extends Thread{
-	@Override
-	public void run() {
-		System.out.println("Number Thread initialized");
-		for(char i = 'A';i<='Z';i++) {
-			System.out.println(i);
-			
-			try {Thread.sleep(500);} catch(Exception e) {}
-		}
-		System.out.println("Number thread End");
-	}
-	
-}
 public class PrintAll {
 
 	public static void main(String[] args) {
