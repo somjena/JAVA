8fb30c3b168264d8111ad37663ab9c288c1cdaab
Soumitra jena <131749408+somjena@users.noreply.github.com>
2025-01-18 18:32:28 +0530

    Create a Thread that will print nummber

---
+++ Oops/src/com/Thread/ThreadClass/PrintNum.java
@@ -0,0 +1,12 @@
+package com.Thread.ThreadClass;
+
+class PrintNum extends Thread{
+	public void run() {
+		System.out.println("Created new Thread");
+		for(int i =1;i<=10;i++) {
+			System.out.println(i);
+			try{Thread.sleep(500);} catch(Exception e) {};
+		}
+		System.out.println();
+	}
+}
