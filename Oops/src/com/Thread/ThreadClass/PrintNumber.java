abd5ecc8b68ab5f746ad61beacff45337df1961b
Soumitra jena <131749408+somjena@users.noreply.github.com>
2025-01-19 21:37:55 +0530

    Update

--- Oops/src/com/Thread/ThreadClass/PrintNumber.java
+++ Oops/src/com/Thread/ThreadClass/PrintNumber.java
@@ -6,7 +6,7 @@ public class PrintNumber {
 
 	public static void main(String[] args) {
 		System.out.println("Main Thread Start");
-		Thread t1 = new PrintNum();
+		Thread t1 = new PrintNum();// Create a Class Using PrintNum
 		t1.start();
 		System.out.println("Main Thread Ends");
 
