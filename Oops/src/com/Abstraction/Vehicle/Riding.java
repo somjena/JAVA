45fcb54213be26106929a6e49add17099094b507
Soumitra jena <131749408+somjena@users.noreply.github.com>
2025-01-21 22:01:21 +0530

    Update

--- Oops/src/com/Abstraction/Vehicle/Riding.java
+++ Oops/src/com/Abstraction/Vehicle/Riding.java
@@ -5,7 +5,10 @@ public class Riding {
 	public static void main(String[] args) {
         
 		Driver.Drive(new BMW());
+		System.out.println("----------------");
 		Driver.Drive(new Benz());
+		System.out.println("----------------");
+		Driver.Drive(new Audi());
 	}
 
 }
