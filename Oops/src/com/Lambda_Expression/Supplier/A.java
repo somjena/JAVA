b28a231e32b653630074ecf0526b119804feaca2
Soumitra jena <131749408+somjena@users.noreply.github.com>
2025-01-31 22:23:11 +0530

    Update

---
+++ Oops/src/com/Lambda_Expression/Supplier/A.java
@@ -0,0 +1,15 @@
+package com.Lambda_Expression.Supplier;
+
+public class A {
+	int i;
+	int j;
+	public A(int i,int j) {
+		this.i=i;
+		this.j=j;
+	}
+	
+	public String toString() {
+		return "i="+i+"j="+j;
+	}
+
+}
