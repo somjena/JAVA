f26dbe6f8346c2dcc7b6adece759a1f77698f085
Soumitra jena <131749408+somjena@users.noreply.github.com>
2025-02-01 23:58:09 +0530

    Stream api Square of Num

---
+++ Oops/src/com/StreamApi/Square.java
@@ -0,0 +1,12 @@
+package com.StreamApi;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Square {
+	public static void main(String[] args) {
+		List<Integer>l=	Arrays.asList(1,2,3,4,5,6,7,8,9);
+		l.stream();
+	}
+
+}
