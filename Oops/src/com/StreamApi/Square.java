0c72bbfe5d8a4cf09e9eeb7b1d39d1f0875821af
Soumitra jena <131749408+somjena@users.noreply.github.com>
2025-02-01 23:59:43 +0530

    update

--- Oops/src/com/StreamApi/Square.java
+++ Oops/src/com/StreamApi/Square.java
@@ -2,11 +2,12 @@ package com.StreamApi;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Square {
 	public static void main(String[] args) {
 		List<Integer>l=	Arrays.asList(1,2,3,4,5,6,7,8,9);
-		l.stream();
+		List l =l.stream().map(i->i*i).collect(Collectors.toList());
 	}
 
 }
