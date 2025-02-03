c821fa99161ccd994c0a6ba314adc1ea04e7e64e
Soumitra jena <131749408+somjena@users.noreply.github.com>
2025-02-03 18:48:33 +0530

    Occurance of Each Word Using Stream Api

---
+++ Oops/src/com/StreamApi/OccuranceofEachWord.java
@@ -0,0 +1,20 @@
+package com.StreamApi;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class OccuranceofEachWord {
+
+	public static void main(String[] args) {
+		String s = "java is java is very easy";
+		String []str=s.split(" ");
+		List<String>l=Arrays.asList(str);
+		Map<Object,Long> m=l.stream().collect(Collectors.groupingBy(c->c,Collectors.counting()));
+		m.forEach((c,i)->System.out.println(c+"="+i));;
+		
+
+	}
+
+}
