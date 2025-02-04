68f5af26b373d44e67a59777223087221347e2db
Soumitra jena <131749408+somjena@users.noreply.github.com>
2025-02-04 20:02:21 +0530

    Current Date By Using Date and Time Api

---
+++ Oops/src/com/DateTimeApi/CurrentDate.java
@@ -0,0 +1,11 @@
+package com.DateTimeApi;
+
+import java.time.LocalDate;
+
+public class CurrentDate {
+	public static void main(String[] args) {
+		LocalDate l = LocalDate.now();
+		System.out.println(l);
+	}
+
+}
