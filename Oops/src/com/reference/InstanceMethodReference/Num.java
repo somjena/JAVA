48c720e14a7160d9104dbcf6b7f3ec050f5fb44f
Soumitra jena <131749408+somjena@users.noreply.github.com>
2025-02-04 20:55:16 +0530

    Num Class

---
+++ Oops/src/com/reference/InstanceMethodReference/Num.java
@@ -0,0 +1,11 @@
+package com.reference.InstanceMethodReference;
+
+public class Num {
+	boolean isEven(int i) {
+		return i%2==0;
+	}
+	void printData(String s) {
+		System.out.println("My name is: "+s);
+	}
+
+}
