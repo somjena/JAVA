76cf52e77708c9229188a752d5e67d781a944186
Soumitra Jena <jenasoumitrapvt@gmail.com>
2025-02-19 21:12:53 +0530

    update

---
+++ Daily_Practice/src/com/Dsa/ArrayList/ArrayslistDriver.java
@@ -0,0 +1,24 @@
+package com.Dsa.ArrayList;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class ArrayslistDriver {
+	public static void main(String[] args) {
+		ArrayList<Integer>a= new ArrayList<Integer>();;
+		System.out.println(a.isEmpty());
+	/*	a.add(10);
+		a.add(20);
+		a.add(30);
+		a.add(2, 40);
+		a.Remove(2);
+		System.out.println(a.size());
+		System.out.println(a.isEmpty());
+		a.traverse();
+		int res = a.get(1);
+		System.out.println(res);*/
+		Set<Integer> s = new HashSet<Integer>();
+		s.add(10);
+	}
+
+}
