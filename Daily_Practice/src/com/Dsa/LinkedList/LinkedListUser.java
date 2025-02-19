a0a053be99b0de4b58d695f29a687e6c99c88850
Soumitra Jena <jenasoumitrapvt@gmail.com>
2025-02-19 21:13:06 +0530

    update

---
+++ Daily_Practice/src/com/Dsa/LinkedList/LinkedListUser.java
@@ -0,0 +1,13 @@
+package com.Dsa.LinkedList;
+
+public class LinkedListUser {
+	public static void main(String[] args) {
+		LinkedList l = new LinkedList();
+		System.out.println(l.isEmpty());
+		l.add(12);
+		l.add(20);
+		System.out.println(l.isEmpty());
+		System.out.println(l.size());
+	}
+
+}
