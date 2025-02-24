1b9a3ecfaa36700ba93d9c2525eae2921224e9af
Soumitra Jena <jenasoumitrapvt@gmail.com>
2025-02-24 20:12:21 +0530

    Node Class

---
+++ Daily_Practice/src/com/BinaryTree/Node.java
@@ -0,0 +1,17 @@
+package com.BinaryTree;
+
+public class Node {
+	int key;
+	Node left;
+	Node right;
+	public Node(int key, Node left, Node right) {
+		this.key = key;
+		this.left = left;
+		this.right = right;
+	}
+	public Node(int key) {
+		this.key=key;
+	}
+	
+
+}
