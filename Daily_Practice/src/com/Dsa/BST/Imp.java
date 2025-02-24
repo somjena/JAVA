ba85601bd88d2c0e6b13901b9dcecd3c138df9c4
Soumitra Jena <jenasoumitrapvt@gmail.com>
2025-02-24 20:12:46 +0530

    implement

---
+++ Daily_Practice/src/com/Dsa/BST/Imp.java
@@ -0,0 +1,13 @@
+package com.Dsa.BST;
+
+public class Imp {
+	public static void main(String[] args) {
+		BinarySearchTree bs = new BinarySearchTree();
+		bs.add(10);
+		bs.add(7);
+		bs.add(3);
+		Node fs =bs.root;
+		System.out.println(fs);
+	}
+
+}
