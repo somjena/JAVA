c0c013adb50cb4d84dac665c7b24ffcb5cc28a5c
somjena <jenasoumitrapvt@gmail.com>
2025-03-04 23:43:22 +0530

    Update

--- Daily_Practice/src/com/String/SumOfNumberinString.java
+++ Daily_Practice/src/com/String/SumOfNumberinString.java
@@ -3,7 +3,7 @@ package com.String;
 public class SumOfNumberinString {
 	public static void main(String[] args) {
 		int Sum=0;
-		String s = "rintu1234";
+		String s = "1rintu1234";
 		for(int i=0;i<s.length();i++) {
 			if(s.charAt(i)>='0'&s.charAt(i)<='9') {
 			Sum+=s.charAt(i)-'0';
