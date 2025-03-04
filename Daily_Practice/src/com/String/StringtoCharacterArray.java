74c904b2ea4d4c2bf3902f743b1d86966999e9c9
somjena <jenasoumitrapvt@gmail.com>
2025-03-04 23:39:10 +0530

    Update StringtoCharacterArray.java
    Update

--- Daily_Practice/src/com/String/StringtoCharacterArray.java
+++ Daily_Practice/src/com/String/StringtoCharacterArray.java
@@ -4,8 +4,9 @@ public class StringtoCharacterArray {
 
 	public static void main(String[] args) {
 		char[]ch= convertCharArray("rintu");
-		
-
+		for(char c:ch) {
+			System.out.println(c);
+		}
 	}
 	public static char[]convertCharArray(String s){
 		char[]ch = new char[s.length()];
