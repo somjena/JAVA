6feaad0aaeb38b10b1a375b05ceec0d949dd5da1
somjena <jenasoumitrapvt@gmail.com>
2025-03-10 22:45:32 +0530

    SubsString name

---
+++ Oops/src/com/StringPrograms/Substringname.java
@@ -0,0 +1,11 @@
+package com.StringPrograms;
+
+public class Substringname {
+	public static void main(String[] args) {
+		String s = "Ramana";
+		for(int i=0;i<s.length();i++) {
+			System.out.println(s.substring(0,i+1));
+		}
+	}
+
+}
