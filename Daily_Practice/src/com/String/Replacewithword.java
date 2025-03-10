10dc6ae6e6d371104b31b4d599813589cbf2de2a
somjena <jenasoumitrapvt@gmail.com>
2025-03-10 22:44:15 +0530

    Replace with word

---
+++ Daily_Practice/src/com/String/Replacewithword.java
@@ -0,0 +1,10 @@
+package com.String;
+
+public class Replacewithword {
+	public static void main(String[] args) {
+		String s = "i love python";
+		String newstring = s.replace("python", "java");
+		System.out.println(newstring);
+	}
+
+}
