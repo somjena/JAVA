953653a5b312f169cb779063baca83eaed7c519c
somjena <jenasoumitrapvt@gmail.com>
2025-03-15 21:00:27 +0530

    Update

--- DataStructures/src/com/SortingAlgorithm/BubbleSort.java
+++ DataStructures/src/com/SortingAlgorithm/BubbleSort.java
@@ -11,10 +11,9 @@ public static void main(String[] args) {
 
 public static void sort(int[]a){
 	for(int i=0;i<a.length-1;i++ ) {
-		for (int j=0;j<a.length-1-i;j++) {
+		for (int j=0;j<a.length-i-1;j++) {
 			if(a[j]>a[j+1]) {
 				int temp=a[j];
-					System.out.println(temp);
 				a[j]=a[j+1];
 				a[j+1]=temp;
 			}
