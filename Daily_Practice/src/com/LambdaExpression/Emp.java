a43da5faf722404cce1e5790de6a196559ad517e
somjena <jenasoumitrapvt@gmail.com>
2025-04-06 12:30:00 +0530

    Emp Class

---
+++ Daily_Practice/src/com/LambdaExpression/Emp.java
@@ -0,0 +1,16 @@
+package com.LambdaExpression;
+
+public class Emp {
+	String name;
+	int age;
+	double sal;
+	public Emp(String name, int age, double sal) {
+		super();
+		this.name = name;
+		this.age = age;
+		this.sal = sal;
+	}
+	
+	
+
+}
