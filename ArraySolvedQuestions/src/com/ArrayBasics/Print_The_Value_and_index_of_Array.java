bfaa642ddbb5c000499f84be7b18d3f38aa25a0e
somjena <jenasoumitrapvt@gmail.com>
2025-04-07 10:30:41 +0530

    Print the Value and  index of array

---
+++ ArraySolvedQuestions/src/com/ArrayBasics/Print_The_Value_and_index_of_Array.java
@@ -0,0 +1,13 @@
+package com.ArrayBasics;
+
+public class Print_The_Value_and_index_of_Array {
+	public static void main(String[] args) {
+		int []a = {1,2,3,4,5,6};
+		for(int i=0;i<a.length;i++) {
+			System.out.println("index "+i+" Value "+ a[i]);
+		}
+		
+	}
+	
+
+}
