34950caf164bf78cade61cd9bda79b161d382b40
somjena <jenasoumitrapvt@gmail.com>
2025-04-08 20:20:08 +0530

    Update the Size Method

--- Daily_Practice/src/com/Dsa/Queue/Queue.java
+++ Daily_Practice/src/com/Dsa/Queue/Queue.java
@@ -8,15 +8,21 @@ public class Queue {
 		a=new Object[5];
 		pos=0;
 	}
-	
+	//add Method
 	public boolean add(Object ele) {
 		if(pos>=a.length)increase();
 		a[pos++]=ele;
 		return true;
 	}
+	//increase method
 	private void increase() {
 		Object []temp = new Object[a.length+3];
 		System.arraycopy(a, 0, temp, 0, a.length);
 		a=temp;
 	}
+	//size method
+	public int size() {
+		return pos;
+	}
+	
 }
