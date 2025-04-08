c6ac5c1e0de9d6d3ba8d71432cecc5c4879af6e8
somjena <jenasoumitrapvt@gmail.com>
2025-04-08 20:00:02 +0530

    Update the test programs

--- Daily_Practice/src/com/Dsa/Stack/Stack.java
+++ Daily_Practice/src/com/Dsa/Stack/Stack.java
@@ -1,5 +1,7 @@
 package com.Dsa.Stack;
 
+import java.util.EmptyStackException;
+
 public class Stack {
 	private Object []a;
 	private int pos;
@@ -15,11 +17,34 @@ public class Stack {
 		a[pos++]=ele;
 		return true;
 	}
-	public void increase() {
+	//increase method
+	private void increase() {
+		Object temp[]= new Object[a.length+3];
+		System.arraycopy(a,0,temp,0,a.length);
+		a=temp;
 		
 	}
-	
+	//size Method 
 	public int size() {
 		return pos;
+	}
+	//peek method
+	public Object peek() {
+		if(pos==0) {
+			throw new EmptyStackException();
+		}
+		return a[size()-1];
+	}
+	//pop Method
+	public Object pop() {
+		if(pos==0) {
+			throw new EmptyStackException();
+		}
+		Object temp = a[size()-1];
+		Object cons[]=new Object[a.length-1];
+		System.arraycopy(a, 0, cons, 0, a.length-1);
+		a=cons;
+		pos--;
+		return temp;
 	}
 }
