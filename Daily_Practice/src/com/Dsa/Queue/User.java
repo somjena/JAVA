86fb74d4215505fda485b0fab98cf64390cda797
somjena <jenasoumitrapvt@gmail.com>
2025-04-08 20:17:37 +0530

    User Program

---
+++ Daily_Practice/src/com/Dsa/Queue/User.java
@@ -0,0 +1,14 @@
+package com.Dsa.Queue;
+
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+public class User {
+
+	public static void main(String[] args) {
+	Queue q = new ArrayDeque();
+	System.out.println(q.poll());
+	System.out.println(q.peek());
+	}
+
+}
