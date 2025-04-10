f2c494048d5c9659a19455f835fba50acabb0c06
somjena <jenasoumitrapvt@gmail.com>
2025-04-10 22:09:43 +0530

    Implement in the User Class

--- Daily_Practice/src/com/Dsa/Queue/User.java
+++ Daily_Practice/src/com/Dsa/Queue/User.java
@@ -1,14 +1,18 @@
 package com.Dsa.Queue;
-
-import java.util.ArrayDeque;
-import java.util.Queue;
-
 public class User {
 
-	public static void main(String[] args) {
-	Queue q = new ArrayDeque();
-	System.out.println(q.poll());
+public static void main(String[] args) {
+	Queue q=  new Queue();
+	q.add(10);
+	q.add(20);
 	System.out.println(q.peek());
+	System.out.println(q.size());
+	System.out.println(q.poll());
+	System.out.println(q.size());
+	
+	System.out.println(q.poll());
+	
+	
 	}
 
 }
