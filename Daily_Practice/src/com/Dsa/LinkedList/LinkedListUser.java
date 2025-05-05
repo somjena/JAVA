402def6b9a9f49cd64bbeca79748e813652f78de
somjena <jenasoumitrapvt@gmail.com>
2025-05-05 21:15:57 +0530

    LinkedListUser.java

--- Daily_Practice/src/com/Dsa/LinkedList/LinkedListUser.java
+++ Daily_Practice/src/com/Dsa/LinkedList/LinkedListUser.java
@@ -3,13 +3,14 @@ package com.Dsa.LinkedList;
 public class LinkedListUser {
 	public static void main(String[] args) {
 		LinkedList l = new LinkedList();
-		System.out.println(l.isEmpty());
-		l.add(12);
+		l.add(10);
 		l.add(20);
-		//System.out.println(l.remove(0));
-		//System.out.println(l.remove(1));
+		l.add(40);
+		l.add(50);
 		l.display();
-		System.out.println(l.get(1));
+		System.out.println("---------------------");
+		l.add(2, 30);
+		l.display();
 		
 	}
 
