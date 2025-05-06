2dc7b51e8ec8dce28f6d93aad258388c528d2eb9
somjena <jenasoumitrapvt@gmail.com>
2025-05-06 14:05:12 +0530

    Node Class

---
+++ Daily_Practice/src/com/Dsa/Stack/Node.java
@@ -0,0 +1,15 @@
+package com.Dsa.Stack;
+
+public class Node {
+	Object a;
+	Node next;
+	
+	public Node(Object a) {
+	       this.a=a;
+	}
+	public Node(Object a,Node next) {
+		this.a=a;
+		this.next=next;
+	}
+
+}
