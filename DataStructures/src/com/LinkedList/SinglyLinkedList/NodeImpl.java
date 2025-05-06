2a7a9281217644c57ae3c468e12a66e76b23fbc3
somjena <jenasoumitrapvt@gmail.com>
2025-05-06 14:07:26 +0530

    Update

--- DataStructures/src/com/LinkedList/SinglyLinkedList/NodeImpl.java
+++ DataStructures/src/com/LinkedList/SinglyLinkedList/NodeImpl.java
@@ -21,18 +21,25 @@ public class NodeImpl {
 		
 */
 		LinkedList l = new LinkedList();
+		//l.Add(101, 1);
 		l.Add(10);
 		l.Add(20);
 		l.Add(30);
 		l.Add(40);
 		l.Add(50);
-		l.Add(70, 2);
+		
+	System.out.println(l.size());
+		//l.Add(60,5);
+		//l.Add(70, 2);
 	
 		//l.Ree(3);
-		l.Display();
+		//l.Display();
+		
 		
 		//System.out.println(l.size());
-		//System.out.println(l.get(5));
+		System.out.println(l.get(0));
+		System.out.println(l.get(1));
+		System.out.println(l.get(2));
 		
 		
 		
