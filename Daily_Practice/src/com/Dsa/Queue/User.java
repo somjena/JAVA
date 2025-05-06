a7a2718298ab1dd7b20a68fd17a11f6c6eae21d9
somjena <jenasoumitrapvt@gmail.com>
2025-05-06 14:04:55 +0530

    Update User.java

--- Daily_Practice/src/com/Dsa/Queue/User.java
+++ Daily_Practice/src/com/Dsa/Queue/User.java
@@ -2,15 +2,29 @@ package com.Dsa.Queue;
 public class User {
 
 public static void main(String[] args) {
-	Queue q=  new Queue();
-	q.add(10);
-	q.add(20);
-	System.out.println(q.peek());
-	System.out.println(q.size());
-	System.out.println(q.poll());
-	System.out.println(q.size());
+//	Queue q=  new Queue();
+//	q.add(10);
+//	q.add(20);
+//	System.out.println(q.peek());
+//	System.out.println(q.size());
+//	System.out.println(q.poll());
+//	System.out.println(q.size());
+//	
+//	System.out.println(q.poll());
+//	System.out.println(q.size());
+//	System.out.println(q.poll());
 	
-	System.out.println(q.poll());
+	
+	
+	QueueN n = new QueueN();
+	n.add(10);
+	n.add(20);
+	n.add(30);
+	n.add(40);
+	n.add(50);
+	System.out.println(n.poll());
+	System.out.println(n.size());
+	n.display();
 	
 	
 	}
